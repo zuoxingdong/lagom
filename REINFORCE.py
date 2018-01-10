c16b3ed6ae4193df0f4d8e9e36770dc9afd0ae52
zuoxingdong <zuoxingdong@hotmail.com>
2018-01-10 12:17:17 +0100

    update

--- REINFORCE.py
+++ REINFORCE.py
@@ -1,31 +1,35 @@
 import torch
 from torch.distributions import Categorical
 
+from Agent import Agent
 from utils import normalize_vec
 
-class REINFORCEAgent(object):
+class REINFORCEAgent(Agent):
     def __init__(self, policy, optimizer):
-        self.policy = policy
-        self.optimizer = optimizer
+        super().__init__(policy, optimizer)
         
-    def choose_action(self, state):
+    def choose_action(self, x):
         """
         Action selection according to internal policies
         
         Args:
-            state: A dictionary with keys of different kind of data. 
+            x: A dictionary with keys of different kind of data. 
                     Possible keys: ['observation', 'current_state', 'goal_state']
             
         Returns:
             output: A dictionary with keys of different kind of data.
                     Possible keys: ['action', 'log_prob']
         """
-        # Compute probability distribution over actions via softmax
-        action_probs = self.policy(state)
+        out_policy = self.policy(x)
+        
+        # Unpack output from policy network
+        action_probs = out_policy.get('action_probs', None)
+        state_value = out_policy.get('state_value', None)
+        
         # Sample an action according to categorical distribution
         action_dist = Categorical(action_probs)
+        # Sample an action and calculate its log-probability according to distribution
         action = action_dist.sample()
-        # log-probability of action
         logprob_action = action_dist.log_prob(action)
         
         # Convert action from Variable to scalar value
@@ -53,10 +57,9 @@ class REINFORCEAgent(object):
         
         # Iterate over batch of trajectories
         for epi_data in data_batch:
+            R = epi_data['returns']
             if normalize_r:
-                R = normalize_vec(epi_data['returns'])
-            else:
-                R = epi_data['returns']
+                R = normalize_vec(R)
             
             # Calculate loss for the policy
             policy_loss = []
