9b227bd307c349ebf561119a0b07b483285930b0
zuoxingdong <zuoxingdong@hotmail.com>
2018-04-20 01:14:57 +0200

    add PartialFlattenDict wrapper
    Former-commit-id: be77c483296cdc9d8ef39a1c7bb8419eb92620d9

---
+++ lagom/envs/wrappers/partial_flatten_dict.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from lagom.envs.spaces import Box
+from lagom.envs.wrappers import ObservationWrapper
+
+
+class PartialFlattenDict(ObservationWrapper):
+    """
+    Returns flattened observation from a dictionary space with partial keys into a Box space. 
+    """
+    def __init__(self, env, keys):
+        super().__init__(env)
+        
+        self.keys = keys
+        
+        spaces = self.env.observation_space.spaces
+        assert all([isinstance(space, Box) for space in spaces.values()])  # enforce all Box spaces
+        
+        # Calculate dimensionality
+        shape = (int(np.sum([spaces[key].flat_dim for key in self.keys])), )
+        self.observation_space = Box(low=-np.inf, high=np.inf, shape=shape, dtype=np.float32)
+        
+    def process_observation(self, observation):
+        return np.concatenate([observation[key].ravel() for key in self.keys])
+    
+    @property
+    def observation_space(self):
+        return self.observation_space
