62c61d24358aa2c5308380dd98d5e82708dd7efa
Xingdong Zuo <zuo@x01>
2018-05-14 17:21:21 +0200

    add MDN; add ConvVAE
    Former-commit-id: c55da07dcbab9b36de032e299e07231286fd9a31

--- lagom/core/networks/__init__.py
+++ lagom/core/networks/__init__.py
@@ -1,3 +1,6 @@
-from lagom.core.networks.mlp import MLP
 from lagom.core.networks.cnn import CNN
+from lagom.core.networks.conv_vae import ConvVAE
+from lagom.core.networks.mdn import MDN
+from lagom.core.networks.mlp import MLP
+from lagom.core.networks.transposed_cnn import TransposedCNN
 from lagom.core.networks.vae import VAE
--- lagom/core/networks/vae.py
+++ lagom/core/networks/vae.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from lagom.core.networks import MLP
+from .mlp import MLP
 
 
 class VAE(nn.Module):
