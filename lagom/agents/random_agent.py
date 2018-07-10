370603fb7c7a8eed982573c6d3c0bea720fe4ab4
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-07-10 20:23:48 +0200

    * Add new ES examples
    * Update Agent
    
    * Add RandomPolicy, BaseCategoricalPolicy and BaseGaussianPolicy
    
    * Update runner.transitions
    
    * Add bash to build PyTorch from source
    
    
    Former-commit-id: af4b9043d292e0997aefedf85f0a4b73ee79a836

--- lagom/agents/random_agent.py
+++ lagom/agents/random_agent.py
@@ -1,21 +1,30 @@
-from lagom.agents.base import BaseAgent
+from .base_agent import BaseAgent
 
 
 class RandomAgent(BaseAgent):
     """
-    A random agent select action uniformly from action space
+    A random agent samples action uniformly from action space
     """
-    def __init__(self, env, config=None):
-        self.env = env
+    def __init__(self, env_spec, config=None):
+        self.env_spec = env_spec
         
         super().__init__(config)
         
-    def choose_action(self, obs=None):
+    def choose_action(self, obs):
+        # Randomly sample an action from action space
+        action = self.env_spec.action_space.sample()
+        
         # Dictionary of output data
         output = {}
-        output['action'] = self.env.action_space.sample()
+        output['action'] = action
         
         return output
         
-    def learn(self, batch_data=None):
+    def learn(self, x):
+        pass
+    
+    def save(self, filename):
+        pass
+    
+    def load(self, filename):
         pass
---
+++ lagom/core/policies/base_policy.py
@@ -0,0 +1,54 @@
+class BasePolicy(object):
+    """
+    Base class for the policy. 
+    
+    It receives user-defined network (must be of types in lagom.core.networks) and
+    environment specification (of type EnvSpec). 
+    
+    All inherited subclasses should implement the following functions
+    1. __call__(self, x)
+    2. process_network_output(self, network_out)
+    """
+    def __init__(self, network, env_spec):
+        """
+        Args:
+            network (BaseNetwork): an instantiated user-defined network. 
+            env_spec (EnvSpec): environment specification. 
+        """
+        self.network = network
+        self.env_spec = env_spec
+        
+    def __call__(self, x):
+        """
+        User-defined function to run the policy network given the input. 
+        
+        It should use the forward() function of internal network, e.g. `self.network(x)`
+        
+        Note that it must return a dictionary of output. 
+        
+        Args:
+            x (Tensor): input data. 
+            
+        Returns:
+            out (dict): A dictionary of output data from running the policy network of given input. 
+                It must contain at least one key, 'action'. Other possible keys include 
+                ['action_logprob', 'state_value']
+        """
+        raise NotImplementedError
+        
+    def process_network_output(self, network_out):
+        """
+        User-defined function to support additional processing of the 
+        output from the internal network. 
+        
+        It can also return a processed output. If there is nothing to return, 
+        then one can return an empty disctionary, i.e. {}. 
+        
+        Args:
+            network_out (dict): Dictionary of data returned from forward pass of internal network. 
+            
+        Returns:
+            processed_network_out (dict): A dictionary of processed network output. 
+                It will be returned together in __call__. 
+        """
+        raise NotImplementedError
