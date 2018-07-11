4518ae1e9eeeab0f77731e6e0d3228daaf7d91e9
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-07-11 19:05:06 +0200

    * Update README
    * Update examples
    
    
    Former-commit-id: 1774259f8b152e244ab73c8ae20dbbedce7cba4f [formerly 9cf5a36cd98927f5b0b16ca5d1918f4b34deb533]
    Former-commit-id: aab1b365b9f95009137d683a391b98a7e37f0c63

--- examples/goal_selection/goal_sampler/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseGoalSampler
-from .linear_goal_sampler import LinearGoalSampler
-from .uniform_goal_sampler import UniformGoalSampler
-from .rejection_goal_sampler import RejectionGoalSampler
-from .rejection_l2_goal_sampler import RejectionL2GoalSampler
-from .rejection_astar_goal_sampler import RejectionAstarGoalSampler
-from .sw_ucb_g import SWUCBgGoalSampler
