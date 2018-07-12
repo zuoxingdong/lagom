* Fix policy gradient example: REINFORCE and AC
* Fix REINFORCEAgent and ActorCriticAgent

* Update BaseCategoricalPolicy and BaseGaussianPolicy: add entropy and perlexity

* Fix Runner: detect state value to add V_s to each transition, and add V_s_next to last transition in each trajectory, it needs one more choose_action from the agent with final observation.

examples/policy_gradient/engine.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+import torch
+
 from lagom.engine import BaseEngine
 
 
@@ -19,7 +21,10 @@ class Engine(BaseEngine):
             out_agent = self.agent.learn(D)
             
             # Unpack agent's learning outputs
-            total_loss = out_agent['total_loss'].item()  # item saves memory
+            loss = out_agent['loss'].item()  # item saves memory
+            if 'batch_value_loss' in out_agent:
+                value_loss = torch.stack(out_agent['batch_value_loss']).mean().item()
+            entropy_loss = torch.stack(out_agent['batch_entropy_loss']).mean().item()
             
             # Unpack useful information from trajectory list
             all_returns = [trajectory.all_returns[0] for trajectory in D]
@@ -31,7 +36,12 @@ class Engine(BaseEngine):
                 print(f'Training iteration: {i+1}')
                 print('-'*50)
                 
-                print(f'Total loss: {total_loss}')
+                if 'current_lr' in out_agent:
+                    print(f'Current lr: {out_agent["current_lr"]}')
+                print(f'Loss: {loss}')
+                if 'value_loss' in locals():  # if value_loss is defined above within train()
+                    print(f'Value loss: {value_loss}')
+                print(f'Entropy loss: {entropy_loss}')
                 print(f'Number of trajectories: {len(D)}')
                 print(f'Average Return: {np.mean(all_returns)}')
                 print(f'Average Discounted Return: {np.mean(all_discounted_returns)}')

examples/policy_gradient/policy.py
@@ -9,7 +9,11 @@ from lagom.core.policies import BaseCategoricalPolicy
 class MLP(BaseMLP):
     def make_params(self, config):
         self.fc1 = nn.Linear(in_features=4, out_features=128)
-        self.fc2 = nn.Linear(in_features=128, out_features=2)
+        
+        self.action_head = nn.Linear(in_features=128, out_features=2)
+        
+        if config['use_value']:
+            self.value_head = nn.Linear(in_features=128, out_features=1)
         
     def init_params(self, config):
         gain = nn.init.calculate_gain(nonlinearity='relu')
@@ -17,20 +21,29 @@ class MLP(BaseMLP):
         nn.init.orthogonal_(self.fc1.weight, gain=gain)
         nn.init.constant_(self.fc1.bias, 0.0)
 
-        nn.init.orthogonal_(self.fc2.weight, gain=gain)
-        nn.init.constant_(self.fc2.bias, 0.0)
+        nn.init.orthogonal_(self.action_head.weight, gain=0.01)  # Smaller scale for action head
+        nn.init.constant_(self.action_head.bias, 0.0)
+        
+        if hasattr(self, 'value_head'):
+            nn.init.orthogonal_(self.value_head.weight, gain=1.0)  # no nonlinearity
+            nn.init.constant_(self.value_head.bias, 0.0)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        action_scores = self.fc2(x)
-        
         # Output dictionary
         out = {}
+        
+        x = F.relu(self.fc1(x))
+        
+        action_scores = self.action_head(x)
         out['action_scores'] = action_scores
+        
+        if hasattr(self, 'value_head'):
+            state_value = self.value_head(x)
+            out['state_value'] = state_value
 
         return out
     
 
 class CategoricalPolicy(BaseCategoricalPolicy):
     def process_network_output(self, network_out):
-        return {}
+        return network_out

lagom/agents/reinforce_agent.py
@@ -17,12 +17,18 @@ class REINFORCEAgent(BaseAgent):
         
     def choose_action(self, obs):
         # Convert to Tensor
+        # Note that we assume obs is a single observation, not batched one
+        # so we unsqueeze it with a batch dimension
         if not torch.is_tensor(obs):
             obs = torch.from_numpy(obs).float()
             obs = obs.unsqueeze(0)  # make a batch dimension
             obs = obs.to(self.device)  # move to device
         
         out_policy = self.policy(obs)
+        # Squeeze the single batch dimension of the action
+        for key, val in out_policy.items():
+            if torch.is_tensor(val):
+                out_policy[key] = val.squeeze(0)
                 
         # Dictionary of output data
         output = {}
@@ -32,73 +38,74 @@ class REINFORCEAgent(BaseAgent):
         
     def learn(self, x):
         batch_policy_loss = []
+        batch_entropy_loss = []
+        batch_total_loss = []
         
         # Iterate over list of trajectories
         for trajectory in x:
-            R = trajectory.all_discounted_returns
+            # Get all discounted returns
+            Rs = trajectory.all_discounted_returns
             if self.config['standardize_r']:  # encourage/discourage half of performed actions, i.e. [-1, 1]
-                standardize = Standardize()
-                R = standardize(R)
+                Rs = Standardize()(Rs)
                 
-            # Calculate policy loss for this trajectory (all time steps)
+            # Get all log-probabilities and entropies
+            logprobs = trajectory.all_info('action_logprob')
+            entropies = trajectory.all_info('entropy')
+                
+            # All losses
             policy_loss = []
-            for logprob, r in zip(trajectory.all_info(name='action_logprob'), R):
-                policy_loss.append(-logprob*float(r))  # TODO: supports VecEnv
-            policy_loss = torch.cat(policy_loss).sum()
+            entropy_loss = []
             
-            # Record the policy loss
+            # Estimate policy gradient for all time steps
+            for logprob, entropy, R in zip(logprobs, entropies, Rs):
+                policy_loss.append(-logprob*float(R))  # TODO: supports VecEnv
+                entropy_loss.append(-entropy)
+                
+            # Sum up losses for all time steps
+            policy_loss = torch.stack(policy_loss).sum()
+            entropy_loss = torch.stack(entropy_loss).sum()
+            
+            # Calculate total loss
+            entropy_coef = self.config['entropy_coef']
+            total_loss = policy_loss + entropy_coef*entropy_loss
+            
+            # Record all losses
             batch_policy_loss.append(policy_loss)
+            batch_entropy_loss.append(entropy_loss)
+            batch_total_loss.append(total_loss)
             
-        # Compute total loss (average over trajectories)
-        total_loss = torch.stack(batch_policy_loss).mean()  # use stack because each element is zero-dim tensor
+        # Compute loss (average over trajectories)
+        loss = torch.stack(batch_total_loss).mean()  # use stack because each element is zero-dim tensor
         
         # Zero-out gradient buffer
         self.optimizer.zero_grad()
         # Backward pass and compute gradients
-        total_loss.backward()
-        # Update for one step
+        loss.backward()
+        
+        # Clip gradient norms if required
+        if self.config['max_grad_norm'] is not None:
+            nn.utils.clip_grad_norm_(parameters=self.policy.network.parameters(), 
+                                     max_norm=self.config['max_grad_norm'], 
+                                     norm_type=2)
+        
+        # Decay learning rate if required
+        if hasattr(self, 'lr_scheduler'):
+            self.lr_scheduler.step()
+        
+        # Take a gradient step
         self.optimizer.step()
         
         # Output dictionary for different losses
         # TODO: if no more backprop needed, record with .item(), save memory without store computation graph
         output = {}
-        output['total_loss'] = total_loss
+        output['loss'] = loss
         output['batch_policy_loss'] = batch_policy_loss
+        output['batch_entropy_loss'] = batch_entropy_loss
+        output['batch_total_loss'] = batch_total_loss
+        if hasattr(self, 'lr_scheduler'):
+            output['current_lr'] = self.lr_scheduler.get_lr()
 
         return output
-            
-        """
-        # Iterate over batch of trajectories
-        for epi_data in batch_data:
-            R = epi_data['returns']
-            if self.config['standardize_r']:  # encourage/discourage half of performed actions, i.e. [-1, 1]
-                R = Standardize().process(R)
-            
-            # Calculate loss for the policy
-            policy_loss = []
-            for log_prob, r in zip(epi_data['logprob_actions'], R):
-                policy_loss.append(-log_prob*r)
-            
-            # Batched loss for each episode
-            batch_policy_loss.append(torch.cat(policy_loss).sum())
-            
-        # Compute total loss over the batch (average over batch, i.e. over episodes)
-        total_loss = torch.cat(batch_policy_loss).mean()
-        
-        # Zero-out gradient buffer
-        self.optimizer.zero_grad()
-        # Backward pass and compute gradients
-        total_loss.backward()
-        # Update for one step
-        self.optimizer.step()
-        
-        # Output dictionary for different losses
-        output = {}
-        output['total_loss'] = total_loss
-        output['batch_policy_loss'] = batch_policy_loss
-
-        return output
-        """
     
     def save(self, filename):
         self.policy.network.save(filename)

lagom/runner/runner.py
@@ -51,6 +51,7 @@ class Runner(object):
                 # Note that do not convert to raw value from Tensor here
                 # Because for Transition object, we often want to record Tensor action to backprop. 
                 action = output_agent['action']
+                state_value = output_agent.get('state_value', None)
                 
                 # Execute the action in the environment
                 if torch.is_tensor(action):  # convert Tensor to raw numpy array
@@ -70,20 +71,32 @@ class Runner(object):
                                         r=reward, 
                                         s_next=obs_next, 
                                         done=done)
+                # Record state value if required
+                if state_value is not None:
+                    transition.add_info('V_s', state_value)
                 # Record additional information from output_agent
                 for key, val in output_agent.items():
-                    if key != 'action':  # action already recorded
+                    if key != 'action' and key != 'state_value':  # action and state_value already recorded
                         transition.add_info(key, val)
                 
                 # Add transition to trajectory
                 trajectory.add_transition(transition)
                 
+                # Back up obs for next iteration to feed into agent
+                obs = obs_next
+                
                 # Terminate if episode finishes
                 if done:
                     break
-                    
-                # Back up obs for next iteration to feed into agent
-                obs = obs_next
+            
+            # Call agent again to compute state value for final obsevation in collected trajectory
+            if state_value is not None:
+                V_s_next = self.agent.choose_action(obs)['state_value']
+                # Set to zero if it is terminal state
+                if done:
+                    V_s_next.fill_(0.0)
+                # Add to the final transition as 'V_s_next'
+                trajectory.transitions[-1].add_info('V_s_next', V_s_next)
             
             # Append trajectory to data
             D.append(trajectory)