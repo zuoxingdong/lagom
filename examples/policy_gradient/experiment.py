e2106e0be9fa40280497c7835fb34e0eb090174b
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-07-16 20:37:54 +0200

    * Update README: add more dependencies
    * Update scripts: add more dependencies
    
    * Add PolySmooth: polynomial fit to smooth curve
    
    * Update examples: policy gradient
    
    * Update setup.py: add more dependencies
    
    * Add BasePlot and CurvePlot

--- examples/policy_gradient/experiment.py
+++ examples/policy_gradient/experiment.py
@@ -20,7 +20,7 @@ class ExperimentMaster(BaseExperimentMaster):
         config = Config()
         
         config.add_grid(name='cuda', val=[True])
-        config.add_item(name='seed', val=1)
+        config.add_grid(name='seed', val=[834748646,  635037080,  984275692,  225168546,  713396787])
         
         config.add_item(name='lr', val=1e-2)
         config.add_item(name='gamma', val=0.995)
@@ -29,7 +29,7 @@ class ExperimentMaster(BaseExperimentMaster):
         config.add_item(name='N', val=1)
         config.add_item(name='T', val=300)
         
-        config.add_item(name='use_value', val=True)  # True for actor-critic
+        config.add_item(name='use_value', val=False)  # True for actor-critic
         config.add_item(name='entropy_coef', val=0.0)
         config.add_item(name='value_coef', val=0.0)
         
@@ -40,42 +40,3 @@ class ExperimentMaster(BaseExperimentMaster):
         configs = config.make_configs()
         
         return configs
-
-"""
-class Experiment(BaseExperiment):
-    def _configure(self):
-        config = GridConfig()
-        
-        config.add('seed', list(range(1)))  # random seeds
-        
-        config.add('hidden_sizes', [[32]])
-        config.add('hidden_nonlinearity', [F.tanh])
-        config.add('lr', [1e-2])  # learning rate of policy network
-        config.add('gamma', [0.995])  # discount factor
-        config.add('GAE_lambda', [0.86])  # GAE lambda
-        config.add('value_coef', [0.5])  # value function loss coefficient
-        config.add('entropy_coef', [0.0])  # policy entropy loss coefficient
-        #config.add('max_grad_norm', [0.5])  # clipping for max gradient norm
-        config.add('T', [10000])  # Max time step per episode
-        config.add('use_optimal_T', [False])  # True: args.T will be modified to optimal steps before rollout for each new goal
-        config.add('predict_value', [True])  # Value function head
-        
-        
-        config.add('train_iter', [50])  # number of training iterations
-        config.add('eval_iter', [1])  # number of evaluation iterations
-        config.add('train_num_epi', [1])  # Number of episodes per training iteration
-        config.add('eval_num_epi', [10])  # Number of episodes per evaluation iteration
-        
-        config.add('init_state', [[6, 1]])  # initial position for each created environment
-        
-        config.add('log_interval', [1])
-        
-        return config.make_configs()
-            
-    def _make_env(self, config):
-        env = gym.make('CartPole-v0')
-        env = GymEnv(env)
-        
-        return env
-    
-"""
