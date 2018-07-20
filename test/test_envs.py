bb71422021791473a53662f2f1ceb11eaefac4fd
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-07-20 14:31:28 +0200

    * Add StackObservation: similar to OpenAI-baselines VecFrameStack, useful for training DQN
    Former-commit-id: e68b0d756b605ea7756f523b071c160790b3d277 [formerly cf32ca41643b2617a6c49da0fee3cb9895872e4c]
    Former-commit-id: 08440c7acdd85b5bd5918fdc69e436501157d502

--- test/test_envs.py
+++ test/test_envs.py
@@ -3,8 +3,11 @@ import numpy as np
 import gym
 
 from lagom.envs import Env
+from lagom.envs import GymEnv
 from lagom.envs import make_gym_env
 from lagom.envs import make_envs
+
+from lagom.envs.wrappers import StackObservation
 
 from lagom import Seeder
 
@@ -28,3 +31,39 @@ class TestEnvs(object):
         raw_env = gym.make('Pendulum-v0')
         raw_env.seed(seeds[0])
         assert np.allclose(env.reset(), raw_env.reset())
+
+
+class TestWrapper(object):
+    def test_stackobservation(self):
+        env = gym.make('Pendulum-v0')
+        env = GymEnv(env)
+        env.seed(0)
+        env = StackObservation(env, 3)
+
+        raw_env = gym.make('Pendulum-v0')
+        raw_env.seed(0)
+
+        init_env = env.reset()
+        init_raw_env = raw_env.reset()
+
+        # Check initial obsevations
+        assert np.allclose(init_env[..., 0], init_raw_env)
+        assert np.allclose(init_env[..., 1:], 0.0)
+
+        # Check one step
+        action = raw_env.action_space.sample()
+        env_obs, _, _, _ = env.step(action)
+        raw_env_obs, _, _, _ = raw_env.step(action)
+
+        assert np.allclose(env_obs[..., 0], raw_env_obs)
+        assert np.allclose(env_obs[..., 1], init_raw_env)
+        assert np.allclose(env_obs[..., 2], 0.0)
+
+        # Check rolling effect of stack observation
+        for _ in range(2):
+            action = raw_env.action_space.sample()
+            env_obs, _, _, _ = env.step(action)
+            raw_env_obs, _, _, _ = raw_env.step(action)
+
+        assert np.allclose(env_obs[..., 0], raw_env_obs)
+        assert not np.allclose(env_obs[..., 2], init_raw_env)
