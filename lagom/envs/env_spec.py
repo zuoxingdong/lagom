442f0e08e222c5203c2d09cd8cd1f0ee75f4ee11
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-07-23 00:57:03 +0200

    Update env_spec.py
    Former-commit-id: dcdc906da38123a0f833add94cdf9ca3fe91e1da

--- lagom/envs/env_spec.py
+++ lagom/envs/env_spec.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from .env import Env
+from .vec_env import VecEnv
 
 
 class EnvSpec(object):
@@ -8,8 +9,8 @@ class EnvSpec(object):
     Create specifications of the environment
     """
     def __init__(self, env):
-        if not isinstance(env, Env):
-            raise TypeError('The object env must be of type lagom.envs.Env.')
+        if not isinstance(env, (Env, VecEnv)):
+            raise TypeError('The object env must be of type lagom Env or VecEnv.')
         
         self.env = env
         
