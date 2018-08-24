4931d2a194ab22a42d82dfd2a1214826aa09202c
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-08-24 16:41:19 +0200

    Update docs
    Former-commit-id: b05b2581a190a5fb45b693ce639d3d7e73eab40d [formerly 0d16a4e69d4d53d93e5e84347d44e62e98087eb3]
    Former-commit-id: a92d01650ace03dac733ddcdbeed957056e3c31e

--- lagom/base_algo.py
+++ lagom/base_algo.py
@@ -1,11 +1,14 @@
 class BaseAlgorithm(object):
-    """Base class for the algorithm. 
+    r"""Base class for all algorithms.
     
-    All inherited subclasses should at least implement the following functions:
-    1. __call__(self, config)
+    Any algorithm should subclass this class. 
+    
+    The subclass should implement at least the following:
+    
+    - __call__(self, config)
     """
     def __init__(self, name):
-        """Initialize the algorithm.
+        r"""Initialize the algorithm.
         
         Args:
             name (str): name of the algorithm
@@ -13,13 +16,13 @@ class BaseAlgorithm(object):
         self.name = name
         
     def __call__(self, config):
-        """Run the algorithm with given configurations. 
+        r"""Run the algorithm with given configuration. 
         
         Args:
             config (OrderedDict): dictionary of configurations
             
         Returns:
             result (object): Result of the execution. 
-                If no need to return anything, then `return None`
+                If no need to return anything, then an ``None`` should be returned. 
         """
         raise NotImplementedError
