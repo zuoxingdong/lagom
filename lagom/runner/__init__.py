6b10ff44215fa4558109902bdfe44b48695f8aa0
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-09-04 17:11:48 +0200

    Update docs

--- lagom/runner/__init__.py
+++ lagom/runner/__init__.py
@@ -1,5 +1,8 @@
 from .transition import Transition
+
+from .base_history import BaseHistory
 from .trajectory import Trajectory
 from .segment import Segment
+
 from .trajectory_runner import TrajectoryRunner
 from .segment_runner import SegmentRunner
--- lagom/runner/trajectory.py
+++ lagom/runner/trajectory.py
@@ -4,86 +4,37 @@ import numpy as np
 
 from lagom.core.transform import ExpFactorCumSum
 
+from .base_history import BaseHistory
 
-class Trajectory(object):
-    """
-    Data for a trajectory, consisting of successive transitions, with additional useful information
-    e.g. other info includes length, success and so on.
+
+class Trajectory(BaseHistory):
+    r"""Define a trajectory, consisting of successive transitions. 
     
-    Note that it is not necessarily an episode (with terminal state). But it must be a part of an episode. 
-    For segment of transitions which can contains `done=True` in the middle (more than one episode data), one 
-    can use Segment instead. 
+    .. note::
+    
+        It is not necessarily a complete episode (final state is terminal state). However, all transitions
+        must come from a single episode. For the history containing transitions from multiple episodes 
+        (i.e. ``done=True`` in the middle), it is recommended to use :class:`Segment` instead. 
+    
+    Example::
+    
     """
-    def __init__(self, gamma):
-        self.gamma = gamma  # discount factor
-        
-        self.transitions = []
-        self.info = {}
-        
     def add_transition(self, transition):
-        """
-        Add a new transition to append in the trajectory
-        
-        Args:
-            transition (Transition): given transition
-        """
-        self.transitions.append(transition)
-        
-    def add_info(self, name, value):
-        """
-        Add additional information for current trajectory
-        
-        Args:
-            name (str): name of the information
-            value (object): value of the information
-        """
-        self.info[name] = value
-        
-    @property
-    def T(self):
-        """
-        Return the current length of the trajectory (number of transitions). 
-        """
-        return len(self.transitions)
+        # Sanity check for trajectory
+        # Not allowed to add more transition if it already contains done=True
+        if len(self.transitions) > 0:  # non-empty
+            assert self.transitions[-1].done == False, 'not allowed to add transition, because already contains done=True'
+        super().add_transition(transition)
     
     @property
     def all_s(self):
-        """
-        Return a list of all states in the trajectory from initial state to last state. 
+        r"""Return a list of all states in the trajectory, from first state to the last state (i.e. ``s_next`` in 
+        last transition). 
         """
         return [transition.s for transition in self.transitions] + [self.transitions[-1].s_next]
     
     @property
-    def all_a(self):
-        """
-        Return a list of all actions in the trajectory. 
-        """
-        return [transition.a for transition in self.transitions]
-    
-    @property
-    def all_r(self):
-        """
-        Return a list of all rewards in the trajectory. 
-        """
-        return [transition.r for transition in self.transitions]
-    
-    @property
-    def all_done(self):
-        """
-        Return a list of all dones in the trajectory. 
-        
-        Note that the done for initial state is not included. 
-        """
-        return [transition.done for transition in self.transitions]
-    
-    @property
     def all_returns(self):
-        r"""
-        Return a list of returns (no discount, gamma=1.0) for all time steps. 
-        
-        Suppose we have all rewards [r_1, ..., r_T], it computes
-        G_t = \sum_{i=t}^{T} r_i
-        """
         return ExpFactorCumSum(1.0)(self.all_r)
     
     @property
