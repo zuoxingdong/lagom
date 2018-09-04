d2d64bfb3425bf7329d59a7179b5339faff3f835
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-09-04 11:43:48 +0200

    Update docs

--- lagom/core/policies/random_policy.py
+++ lagom/core/policies/random_policy.py
@@ -8,23 +8,8 @@ class RandomPolicy(BasePolicy):
     
     Example::
     
-        from lagom.envs import make_gym_env
-        from lagom.envs import make_envs
-        from lagom.envs import EnvSpec
-        from lagom.envs.vec_env import SerialVecEnv
-
-        from lagom.core.policies import RandomPolicy
-
-        list_make_env = make_envs(make_env=make_gym_env, 
-                                  env_id='CartPole-v1', 
-                                  num_env=2, 
-                                  init_seed=0)
-        venv = SerialVecEnv(list_make_env=list_make_env)
-        obs = venv.reset()
-
-        env_spec = EnvSpec(venv)
         policy = RandomPolicy(config=None, network=None, env_spec=env_spec)
-        policy(obs)
+        policy(observation)
         
     """
     def __call__(self, x):
