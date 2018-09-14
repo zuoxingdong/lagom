9b98630459bfd0e56d6e3dc3f2255f5e84e65170
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-09-14 13:18:56 +0200

    Update

--- lagom/core/plotter/__init__.py
+++ lagom/core/plotter/__init__.py
@@ -5,4 +5,5 @@ from .grid_image import GridImage
 try:  # workaround on server without fake screen but still running other things well
     from .image_viewer import ImageViewer
 except ImportError:
-    print('ImageViewer failed to import due to pyglet. ')
+    import warnings
+    warnings.warn('ImageViewer failed to import due to pyglet. ')
