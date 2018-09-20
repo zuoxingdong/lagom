5d5bd563c98b94ccf9454ef96fa5c0993bc55b74
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-09-20 07:42:10 +0200

    Rewrite A2C example
    Former-commit-id: 5d066a578e771161671328f3c803f419dd2e16d1 [formerly e00cfb322a3cb8a977a52eedad7a101b6aa4aaf5]
    Former-commit-id: c564d1a875f7cbd6d2aa1ad5700f2e24c63ee8a5

--- examples/policy_gradient/a2c/engine.py
+++ examples/policy_gradient/a2c/engine.py
@@ -3,21 +3,24 @@ import numpy as np
 import torch
 
 from lagom import Logger
+from lagom import color_str
+
 from lagom.engine import BaseEngine
+
 from lagom.envs import make_gym_env
 from lagom.envs import make_envs
 from lagom.envs.vec_env import SerialVecEnv
-from lagom.envs.vec_env import StandardizeVecEnv
+from lagom.envs.vec_env import VecStandardize
+
 from lagom.runner import TrajectoryRunner
 
 
 class Engine(BaseEngine):
     def train(self, n):
-        # Set network as training mode
-        self.agent.policy.network.train()
+        self.agent.policy.network.train()  # set to train mode
         
         # Collect a list of segments
-        D = self.runner(T=self.config['train:T'])
+        D = self.runner(T=self.config['train.T'])
         
         # Train agent with collected data
         out_agent = self.agent.learn(D)
@@ -29,7 +32,7 @@ class Engine(BaseEngine):
         
         return train_output
     
-    def log_train(self, train_output):
+    def log_train(self, train_output, **kwargs):
         # Create training logger
         logger = Logger(name='train_logger')
         
@@ -38,68 +41,48 @@ class Engine(BaseEngine):
         out_agent = train_output['out_agent']
         n = train_output['n']
         
-        # Loggings
-        # Use item() for tensor to save memory
-        logger.log(key='train_iteration', val=n+1)  # iteration starts from 1
-        if self.config['algo:use_lr_scheduler']:
-            logger.log(key='current_lr', val=out_agent['current_lr'])
+        # Loggings: use item() to save memory
+        logger.log('train_iteration', n+1)  # iteration starts from 1
+        if self.config['algo.use_lr_scheduler']:
+            logger.log('current_lr', out_agent['current_lr'])
             
-        logger.log(key='loss', val=out_agent['loss'].item())
-        policy_loss = torch.stack(out_agent['batch_policy_loss']).mean().item()
-        logger.log(key='policy_loss', val=policy_loss)
-        entropy_loss = torch.stack(out_agent['batch_entropy_loss']).mean().item()
-        logger.log(key='policy_entropy', val=-entropy_loss)  # negation of entropy loss
-        value_loss = torch.stack(out_agent['batch_value_loss']).mean().item()
-        logger.log(key='value_loss', val=value_loss)
+        logger.log('loss', out_agent['loss'])
+        logger.log('policy_loss', out_agent['policy_loss'])
+        logger.log('policy_entropy', -out_agent['entropy_loss'])  # negate entropy loss is entropy
+        logger.log('value_loss', out_agent['value_loss'])
         
-        # Get some data from segment list
+        # Log something about segments
         all_immediate_reward = [segment.all_r for segment in D]
         num_timesteps = sum([segment.T for segment in D])
         
-        # Log more information
-        logger.log(key='num_segments', val=sum([len(segment.split_transitions) for segment in D]))
-        logger.log(key='num_timesteps', val=num_timesteps)
-        logger.log(key='accumulated_trained_timesteps', val=self.agent.accumulated_trained_timesteps)
-        logger.log(key='average_immediate_reward', val=np.mean(all_immediate_reward))
-        logger.log(key='std_immediate_reward', val=np.std(all_immediate_reward))
-        logger.log(key='min_immediate_reward', val=np.min(all_immediate_reward))
-        logger.log(key='max_immediate_reward', val=np.max(all_immediate_reward))
+        logger.log('num_segments', len(D))
+        logger.log('num_subsegments', sum([len(segment.trajectories) for segment in D]))
+        logger.log('num_timesteps', num_timesteps)
+        logger.log('accumulated_trained_timesteps', self.agent.total_T)
+        logger.log('average_immediate_reward', np.mean(all_immediate_reward))
+        logger.log('std_immediate_reward', np.std(all_immediate_reward))
+        logger.log('min_immediate_reward', np.min(all_immediate_reward))
+        logger.log('max_immediate_reward', np.max(all_immediate_reward))
         
-        # Dump the loggings
-        print('-'*50)
-        logger.dump(keys=None, index=None, indent=0)
-        print('-'*50)
-        
-        return logger
+        # Dump loggings
+        if n == 0 or (n+1) % self.config['log.print_interval'] == 0:
+            print('-'*50)
+            logger.dump(keys=None, index=None, indent=0)
+            print('-'*50)
+
+        return logger.logs
         
     def eval(self, n):
-        # Set network as evaluation mode
-        self.agent.policy.network.eval()
+        self.agent.policy.network.eval()  # set to evaluation mode
         
-        # Create a new instance of VecEnv envrionment
-        list_make_env = make_envs(make_env=make_gym_env, 
-                                  env_id=self.config['env:id'], 
-                                  num_env=1, 
-                                  init_seed=self.config['seed'])
-        env = SerialVecEnv(list_make_env)
-        # Wrapper to standardize observation from training scaling of mean and standard deviation
-        if self.config['env:normalize']:
-            env = StandardizeVecEnv(venv=env, 
-                                    use_obs=True, 
-                                    use_reward=False,  # do not standardize reward, use original
-                                    clip_obs=self.runner.env.clip_obs,
-                                    eps=self.runner.env.eps, 
-                                    constant_obs_mean=self.runner.env.obs_runningavg.mu, 
-                                    constant_obs_std=self.runner.env.obs_runningavg.sigma)
-
-        # Create a TrajectoryRunner
-        runner = TrajectoryRunner(agent=self.agent, 
-                                  env=env, 
-                                  gamma=self.config['algo:gamma'])
-        # Evaluate the agent for a set of trajectories
-        # Retrieve pre-defined maximum episode timesteps in the environment
-        T = env.T[0]  # take first one because of VecEnv
-        D = runner(N=self.config['eval:N'], T=T)
+        # synchronize running average if environment wrapped by VecStandardize
+        if self.config['env.standardize']:
+            self.eval_runner.env.constant_obs_mean = self.runner.env.obs_runningavg.mu
+            self.eval_runner.env.constant_obs_std = self.runner.env.obs_runningavg.sigma
+        
+        # Collect a list of trajectories
+        T = self.eval_runner.env.T
+        D = self.eval_runner(N=self.config['eval.N'], T=T)
         
         # Return evaluation output
         eval_output = {}
@@ -109,7 +92,7 @@ class Engine(BaseEngine):
         
         return eval_output
     
-    def log_eval(self, eval_output):    
+    def log_eval(self, eval_output, **kwargs):
         # Create evaluation logger
         logger = Logger(name='eval_logger')
         
@@ -118,26 +101,26 @@ class Engine(BaseEngine):
         n = eval_output['n']
         T = eval_output['T']
         
-        # Compute some metrics
+        # Loggings: use item() to save memory
+        # Log something about trajectories
         batch_returns = [sum(trajectory.all_r) for trajectory in D]
         batch_T = [trajectory.T for trajectory in D]
         
-        # Loggings
-        # Use item() for tensor to save memory
-        logger.log(key='evaluation_iteration', val=n+1)
-        logger.log(key='num_trajectories', val=len(D))
-        logger.log(key='max_allowed_horizon', val=T)
-        logger.log(key='average_horizon', val=np.mean(batch_T))
-        logger.log(key='num_timesteps', val=np.sum(batch_T))
-        logger.log(key='accumulated_trained_timesteps', val=self.agent.accumulated_trained_timesteps)
-        logger.log(key='average_return', val=np.mean(batch_returns))
-        logger.log(key='std_return', val=np.std(batch_returns))
-        logger.log(key='min_return', val=np.min(batch_returns))
-        logger.log(key='max_return', val=np.max(batch_returns))
+        logger.log('evaluation_iteration', n+1)
+        logger.log('num_trajectories', len(D))
+        logger.log('max_allowed_horizon', T)
+        logger.log('average_horizon', np.mean(batch_T))
+        logger.log('num_timesteps', np.sum(batch_T))
+        logger.log('accumulated_trained_timesteps', self.agent.total_T)
+        logger.log('average_return', np.mean(batch_returns))
+        logger.log('std_return', np.std(batch_returns))
+        logger.log('min_return', np.min(batch_returns))
+        logger.log('max_return', np.max(batch_returns))
         
-        # Dump the loggings
-        print('-'*50)
-        logger.dump(keys=None, index=None, indent=0)
-        print('-'*50)
-        
-        return logger
+        # Dump loggings
+        if n == 0 or (n+1) % self.config['log.print_interval'] == 0:
+            print(color_str('+'*50, 'yellow', 'bold'))
+            logger.dump(keys=None, index=None, indent=0)
+            print(color_str('+'*50, 'yellow', 'bold'))
+
+        return logger.logs
--- lagom/agents/a2c_agent.py
+++ lagom/agents/a2c_agent.py
@@ -5,100 +5,108 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from .base_agent import BaseAgent
+
 from lagom.core.transform import Standardize
 
 
 class A2CAgent(BaseAgent):
-    """
-    Advantage Actor-Critic (A2C) with option to use Generalized Advantage Estimate (GAE)
+    r"""`Advantage Actor-Critic`_ (A2C) with option to use Generalized Advantage Estimate (GAE)
     
     The main difference of A2C is to use bootstrapping for estimating the advantage function and training value function. 
     
-    Reference: https://arxiv.org/abs/1602.01783
+    .. _Advantage Actor-Critic:
+        https://arxiv.org/abs/1602.01783
     
-    Note that we use fixed-length segments of experiment to compute returns and advantages. 
-    https://blog.openai.com/baselines-acktr-a2c/
+    Like `OpenAI baselines` we use fixed-length segments of experiment to compute returns and advantages. 
     
-    For this purpose, please use SegmentRunner, not TrajectoryRunner to collect data for A2CAgent. 
+    .. _OpenAI baselines:
+        https://blog.openai.com/baselines-acktr-a2c/
+    
+    .. note::
+    
+        Use :class:`SegmentRunner` to collect data, not :class:`TrajectoryRunner`
+    
     """
-    def __init__(self, policy, optimizer, config, **kwargs):
+    def __init__(self, config, policy, optimizer, **kwargs):
         self.policy = policy
         self.optimizer = optimizer
         
         super().__init__(config, **kwargs)
         
-        self.accumulated_trained_timesteps = 0
+        # accumulated trained timesteps
+        self.total_T = 0
         
     def choose_action(self, obs):
-        # Convert to Tensor
-        # Note that the observation should be batched already (even if only one Segment)
-        if not torch.is_tensor(obs):
-            obs = torch.from_numpy(np.array(obs)).float()
-            obs = obs.to(self.device)  # move to device
+        if not torch.is_tensor(obs):  # Tensor conversion, already batched observation
+            obs = torch.from_numpy(np.asarray(obs)).float().to(self.device)
+            
+        # Call policy: all metrics should be batched properly for Runner to work properly
+        out_policy = self.policy(obs, out_keys=['action', 'action_logprob', 'state_value',
+                                                'entropy', 'perplexity'])
         
-        # Call policy
-        # Note that all metrics should also be batched for SegmentRunner to work properly, check policy/network output.
-        out_policy = self.policy(obs)
-                
-        # Dictionary of output data
-        output = {}
-        output = {**out_policy}
-                
-        return output
+        return out_policy
         
     def learn(self, D):
+        out = {}
+        
         batch_policy_loss = []
         batch_value_loss = []
         batch_entropy_loss = []
         batch_total_loss = []
         
-        # Iterate over list of Segment in D
-        for segment in D:
+        for segment in D:  # iterate over segments
             # Get all boostrapped discounted returns as estimate of Q
             Qs = segment.all_bootstrapped_discounted_returns
-            # TODO: when use GAE of TDs, really standardize it ? biased magnitude of learned value get wrong TD error
-            # Standardize advantage estimates if required
-            # encourage/discourage half of performed actions, respectively.
-            if self.config['agent:standardize']:
-                Qs = Standardize()(Qs)
-            
-            # Get all state values (without V_s_next in final transition)
-            Vs = segment.all_info('V_s')
-            
+        
+            # Standardize: encourage/discourage half of performed actions
+            if self.config['agent.standardize_Q']:
+                Qs = Standardize()(Qs).tolist()
+                
+            # Get all state values and intermediate terminal state and final state
+            Vs, finals = segment.all_V
+            final_Vs, final_dones = zip(*finals)
+            assert len(Vs) == len(segment.transitions)
+                
             # Advantage estimates
-            As = [Q - V.item() for Q, V in zip(Qs, Vs)]
-            
+            As = [Q - V.item() for Q, V in zip(Qs, Vs)]    
+                
             # Get all log-probabilities and entropies
             logprobs = segment.all_info('action_logprob')
             entropies = segment.all_info('entropy')
-            
+        
             # Estimate policy gradient for all time steps and record all losses
             policy_loss = []
-            value_loss = []
             entropy_loss = []
+            value_loss = []
             for logprob, entropy, A, Q, V in zip(logprobs, entropies, As, Qs, Vs):
                 policy_loss.append(-logprob*A)
-                value_loss.append(F.mse_loss(V, torch.tensor(Q).view_as(V).to(V.device)))
                 entropy_loss.append(-entropy)
+                value_loss.append(F.mse_loss(V, torch.tensor(Q).view_as(V).to(V.device)))
+            for final_V, final_done in zip(final_Vs, final_dones):  # learn terminal state value as zero
+                if final_done:
+                    value_loss.append(F.mse_loss(final_V, torch.tensor(0.0).view_as(V).to(V.device)))
         
             # Average over losses for all time steps
             policy_loss = torch.stack(policy_loss).mean()
-            value_loss = torch.stack(value_loss).mean()
             entropy_loss = torch.stack(entropy_loss).mean()
+            value_loss = torch.stack(value_loss).mean()
         
             # Calculate total loss
-            value_coef = self.config['agent:value_coef']
-            entropy_coef = self.config['agent:entropy_coef']
+            entropy_coef = self.config['agent.entropy_coef']
+            value_coef = self.config['agent.value_coef']
             total_loss = policy_loss + value_coef*value_loss + entropy_coef*entropy_loss
-            
+        
             # Record all losses
             batch_policy_loss.append(policy_loss)
-            batch_value_loss.append(value_loss)
             batch_entropy_loss.append(entropy_loss)
+            batch_value_loss.append(value_loss)
             batch_total_loss.append(total_loss)
         
-        # Compute loss (average over segments)
-        loss = torch.stack(batch_total_loss).mean()  # use stack because each element is zero-dim tensor
+        # Compute loss (average over segments): use `stack` as each is zero-dim
+        loss = torch.stack(batch_total_loss).mean()
+        policy_loss = torch.stack(batch_policy_loss).mean()
+        entropy_loss = torch.stack(batch_entropy_loss).mean()
+        value_loss = torch.stack(batch_value_loss).mean()
         
         # Zero-out gradient buffer
         self.optimizer.zero_grad()
@@ -106,59 +114,38 @@ class A2CAgent(BaseAgent):
         loss.backward()
         
         # Clip gradient norms if required
-        if self.config['agent:max_grad_norm'] is not None:
+        if self.config['agent.max_grad_norm'] is not None:
             nn.utils.clip_grad_norm_(parameters=self.policy.network.parameters(), 
-                                     max_norm=self.config['agent:max_grad_norm'], 
+                                     max_norm=self.config['agent.max_grad_norm'], 
                                      norm_type=2)
         
         # Decay learning rate if required
         if hasattr(self, 'lr_scheduler'):
-            if 'train:iter' in self.config:  # iteration-based training, so just increment epoch by default
+            if 'train.iter' in self.config:  # iteration-based training, so just increment epoch by default
                 self.lr_scheduler.step()
-            elif 'train:timestep' in self.config:  # timestep-based training, increment with timesteps
-                self.lr_scheduler.step(self.accumulated_trained_timesteps)
+            elif 'train.timestep' in self.config:  # timestep-based training, increment with timesteps
+                self.lr_scheduler.step(self.total_T)
             else:
-                raise KeyError('expected train:iter or train:timestep in config, but none of them exist')
+                raise KeyError('expected `train.iter` or `train.timestep` in config, but none of them exist')
         
         # Take a gradient step
         self.optimizer.step()
         
         # Accumulate trained timesteps
-        self.accumulated_trained_timesteps += sum([segment.T for segment in D])
+        self.total_T += sum([segment.T for segment in D])
         
-        # Output dictionary for different losses
-        # TODO: if no more backprop needed, record with .item(), save memory without store computation graph
-        output = {}
-        output['loss'] = loss
-        output['batch_policy_loss'] = batch_policy_loss
-        output['batch_value_loss'] = batch_value_loss
-        output['batch_entropy_loss'] = batch_entropy_loss
-        output['batch_total_loss'] = batch_total_loss
+        # Output dictionary: use `item()` to save memory if no more backprop needed
+        out['loss'] = loss.item()
+        out['policy_loss'] = policy_loss.item()
+        out['entropy_loss'] = entropy_loss.item()
+        out['value_loss'] = value_loss.item()
         if hasattr(self, 'lr_scheduler'):
-            output['current_lr'] = self.lr_scheduler.get_lr()
-        
-        return output
-    
-    def save(self, filename):
-        self.policy.network.save(filename)
-    
-    def load(self, filename):
-        self.policy.network.load(filename)
+            out['current_lr'] = self.lr_scheduler.get_lr()
 
-        
-"""
-            
-            # Generalized Advantage Estimation (GAE)
-            all_TD = episode.all_TD
-            alpha = episode.gamma*self.config['GAE_lambda']
-            GAE_advantages = ExponentialFactorCumSum(alpha=alpha).process(all_TD)
-            # Standardize advantages to [-1, 1], encourage/discourage half of actions
-            GAE_advantages = Standardize().process(GAE_advantages)
-            
-            
-            for logprob, V, Q, GAE_advantage, entropy in zip(log_probs, Vs, Qs, GAE_advantages, entropies):
-                policy_loss.append(-logprob*GAE_advantage)
-                value_loss.append(F.mse_loss(V, torch.Tensor([Q]).unsqueeze(0)).unsqueeze(0))
-                entropy_loss.append(-entropy)
-           
-"""
+        return out
+    
+    def save(self, f):
+        self.policy.network.save(f)
+    
+    def load(self, f):
+        self.policy.network.load(f)
