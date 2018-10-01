67a4dc4d79ba655f76860c2f14b70929662254dc
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-10-01 17:54:17 +0200

    Update all agents: support LSTM policy; clean code with removal of obvious comments

--- lagom/agents/reinforce_agent.py
+++ lagom/agents/reinforce_agent.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils import clip_grad_norm_
 
 from .base_agent import BaseAgent
 
@@ -11,43 +12,45 @@ from lagom.core.transform import Standardize
 
 class REINFORCEAgent(BaseAgent):
     r"""REINFORCE agent (no baseline). """
-    def __init__(self, config, policy, optimizer, **kwargs):
+    def __init__(self, config, device, policy, optimizer, **kwargs):
         self.policy = policy
         self.optimizer = optimizer
         
-        super().__init__(config, **kwargs)
+        super().__init__(config, device, **kwargs)
         
         # accumulated trained timesteps
         self.total_T = 0
         
-    def choose_action(self, obs):
-        if not torch.is_tensor(obs):  # Tensor conversion, already batched observation
-            obs = torch.from_numpy(np.asarray(obs)).float().to(self.device)
+    def choose_action(self, obs, info={}):
+        if not torch.is_tensor(obs):
+            obs = np.asarray(obs)
+            assert obs.ndim >= 2, f'expected at least 2-dim for batched data, got {obs.ndim}'
+            obs = torch.from_numpy(obs).float().to(self.device)
             
-        # Call policy: all metrics should be batched properly for Runner to work properly
-        out_policy = self.policy(obs, out_keys=['action', 'action_logprob', 
-                                                'entropy', 'perplexity'])
+        if self.policy.recurrent and self.info['reset_rnn_states']:
+            self.policy.reset_rnn_states(batch_size=obs.size(0))
+            self.info['reset_rnn_states'] = False  # Done, turn off
+            
+        out_policy = self.policy(obs, 
+                                 out_keys=['action', 'action_logprob', 
+                                           'entropy', 'perplexity'], 
+                                 info=info)
         
         return out_policy
 
-    def learn(self, D):
-        out = {}
-        
+    def learn(self, D, info={}):
         batch_policy_loss = []
         batch_entropy_loss = []
         batch_total_loss = []
         
-        for trajectory in D:  # iterate over trajectories
-            # Get all discounted returns as estimate of Q
+        for trajectory in D:
+            logprobs = trajectory.all_info('action_logprob')
+            entropies = trajectory.all_info('entropy')
             Qs = trajectory.all_discounted_returns
             
             # Standardize: encourage/discourage half of performed actions
             if self.config['agent.standardize_Q']:
                 Qs = Standardize()(Qs).tolist()
-            
-            # Get all log-probabilities and entropies
-            logprobs = trajectory.all_info('action_logprob')
-            entropies = trajectory.all_info('entropy')
             
             # Estimate policy gradient for all time steps and record all losses
             policy_loss = []
@@ -56,7 +59,7 @@ class REINFORCEAgent(BaseAgent):
                 policy_loss.append(-logprob*Q)
                 entropy_loss.append(-entropy)
                 
-            # Average over losses for all time steps
+            # Average losses over all time steps
             policy_loss = torch.stack(policy_loss).mean()
             entropy_loss = torch.stack(entropy_loss).mean()
             
@@ -69,38 +72,34 @@ class REINFORCEAgent(BaseAgent):
             batch_entropy_loss.append(entropy_loss)
             batch_total_loss.append(total_loss)
             
-        # Compute loss (average over trajectories): use `stack` as each is zero-dim
-        loss = torch.stack(batch_total_loss).mean()
+        # Average loss over list of Trajectory
         policy_loss = torch.stack(batch_policy_loss).mean()
         entropy_loss = torch.stack(batch_entropy_loss).mean()
+        loss = torch.stack(batch_total_loss).mean()
         
-        # Zero-out gradient buffer
+        # Train with estimated policy gradient
         self.optimizer.zero_grad()
-        # Backward pass and compute gradients
         loss.backward()
         
-        # Clip gradient norms if required
         if self.config['agent.max_grad_norm'] is not None:
-            nn.utils.clip_grad_norm_(parameters=self.policy.network.parameters(), 
-                                     max_norm=self.config['agent.max_grad_norm'], 
-                                     norm_type=2)
-        
-        # Decay learning rate if required
+            clip_grad_norm_(parameters=self.policy.network.parameters(), 
+                            max_norm=self.config['agent.max_grad_norm'], 
+                            norm_type=2)
+            
         if hasattr(self, 'lr_scheduler'):
-            if 'train.iter' in self.config:  # iteration-based training, so just increment epoch by default
+            if 'train.iter' in self.config:  # iteration-based
                 self.lr_scheduler.step()
-            elif 'train.timestep' in self.config:  # timestep-based training, increment with timesteps
+            elif 'train.timestep' in self.config:  # timestep-based
                 self.lr_scheduler.step(self.total_T)
             else:
-                raise KeyError('expected `train.iter` or `train.timestep` in config, but none of them exist')
-        
-        # Take a gradient step
+                raise KeyError('expected `train.iter` or `train.timestep` in config, but got none of them')
+                
         self.optimizer.step()
         
         # Accumulate trained timesteps
         self.total_T += sum([trajectory.T for trajectory in D])
         
-        # Output dictionary: use `item()` to save memory if no more backprop needed
+        out = {}
         out['loss'] = loss.item()
         out['policy_loss'] = policy_loss.item()
         out['entropy_loss'] = entropy_loss.item()
