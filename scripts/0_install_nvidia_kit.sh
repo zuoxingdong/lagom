51ae56f4bed93f438f77011c0ec845a624573836
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-10-11 17:43:38 +0200

    Update scripts: cleaner design and all dependencies in requirements.txt
    Former-commit-id: 19905b8c1a58a389246d2e7ea6e806ef65ea0cb2 [formerly b3aa916f0517b8e678fa3ba35de94635e3f4ce94]
    Former-commit-id: 95602dbc115ff9fc32d1c64b641cd51339be8a81

--- scripts/0_install_nvidia_kit.sh
+++ scripts/0_install_nvidia_kit.sh
@@ -3,24 +3,22 @@
 
 ########################################
 # Install NVIDIA Drivers/CUDA/cuDNN    #
-# Date: 2018-08-10                     #
+# Date: 2018-10-11                     #
 # Author: Xingdong Zuo                 #
 ########################################
 
 # Stop when some intermediate command is failed
 set -e
 
-# Create list of variables
-# Find corresponding Driver runfile from Advanced Driver Search (with Beta version)
-# http://www.nvidia.com/Download/Find.aspx
+# Download GPU Driver file: http://www.nvidia.com/Download/Find.aspx
 export DRIVER_URL=http://us.download.nvidia.com/XFree86/Linux-x86_64/396.51/NVIDIA-Linux-x86_64-396.51.run
 export DRIVER_RUNFILE=NVIDIA-Linux-x86_64-396.51.run
-# Find CUDA runfile from NVIDIA website
-# https://developer.nvidia.com/cuda-downloads
+export DRIVER_VERSION=396
+# Download CUDA runfile: https://developer.nvidia.com/cuda-downloads
 export CUDA_URL=https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux
 export CUDA_RUNFILE=cuda_9.2.148_396.37_linux
 export CUDA_VERSION=9.2
-# cuDNN file: Download from https://developer.nvidia.com/rdp/cudnn-download
+# Download cuDNN file: https://developer.nvidia.com/rdp/cudnn-download
 export CUDNN_FILE=cudnn-9.2-linux-x64-v7.2.1.38.tgz
 
 # Disable nouveau driver: otherwise distribution provided pre-install will fail
@@ -59,6 +57,13 @@ wget $DRIVER_URL
 chmod +x ./$DRIVER_RUNFILE  # make it executable
 sudo ./$DRIVER_RUNFILE --dkms --no-opengl-files  # no OpenGL to support xvfb for fake screen on server
 echo 'NVIDIA driver installation is completed !'
+
+# Append environment variables to .bashrc
+echo "# Appended by Nvidia Driver installer" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/lib/nvidia-$DRIVER_VERSION/bin/" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/nvidia-$DRIVER_VERSION/" >> ~/.bashrc
+echo ""  >> ~/.bashrc
+. ~/.bashrc  # refresh bashrc
 
 # Remove driver runfile
 rm ./$DRIVER_RUNFILE
--- scripts/3_setup_envs.sh
+++ scripts/3_setup_envs.sh
@@ -2,64 +2,24 @@
 
 #############################
 # Install dependencies      #
-# Date: 2018-08-10  	    #
+# Date: 2018-10-11  	    #
 # Author: Xingdong Zuo      #
 #############################
 
 # Stop when some intermediate command is failed
 set -e
 
-# Create list of variables
-export ENV_NAME=RL  # name of Anaconda environment
-export NVIDIA_DRIVER_VER=396  # major version of Nvidia driver
-
 # Alias of conda command
 export CONDA=$HOME/anaconda3/bin/conda
-
-# Update all installed packages
-$CONDA update --all
-
-# Append more useful things to bashrc
-echo "# PATH" >> ~/.bashrc
-echo "export PATH=/usr/lib/nvidia-$NVIDIA_DRIVER_VER/bin/:\$PATH" >> ~/.bashrc
-
-echo "# LIBRARY" >> ~/.bashrc
-echo "export LIBRARY_PATH=$HOME/anaconda3/envs/$ENV_NAME/lib:\$LIBRARY_PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/lib/nvidia-$NVIDIA_DRIVER_VER:$HOME/anaconda3/envs/$ENV_NAME/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$HOME/.mujoco/mjpro150/bin" >> ~/.bashrc 
-
-echo "# PKG_CONFIG" >> ~/.bashrc
-echo "export PKG_CONFIG_PATH=\$PKG_CONFIG_PATH:$HOME/anaconda3/envs/$ENV_NAME/lib/pkgconfig/" >> ~/.bashrc
 
 # Set up .vimrc for tab as 4 spaces
 echo "set tabstop=4" >> ~/.vimrc
 echo "set shiftwidth=4" >> ~/.vimrc
 echo "set expandtab" >> ~/.vimrc
 
-# Install IPython to enforce all later command in Python 3 context
-$CONDA install -y ipython
+# Install dependencies
+$CONDA update --all
+$CONDA install -y ipython patchelf
 
-# Upgrade pip and install some dependencies
 pip install --upgrade pip
-pip install cmake cython msgpack
-pip install setuptools wheel twine
-pip install pyglet
-
-# Install some conda packages
-$CONDA install -y numpy scipy matplotlib scikit-image scikit-learn
-
-# From PyTorch Github README
-$CONDA install -y pyyaml mkl mkl-include cffi typing
-
-# Install patchelf useful for mujoco-py to work, potential problem with Ubuntu 16.04
-# TODO: test on Ubuntu 18.04
-$CONDA install -y patchelf
-
-# Install some pip packages
-pip install pandas seaborn
-pip install jupyterlab cloudpickle
-pip install mujoco-py gym[all] cma
-pip install pytest flake8 sphinx sphinx_rtd_theme
-
-# PyTorch
-$CONDA install pytorch torchvision cuda92 -c pytorch
+pip install -r ../requirements.txt
