a72562d9f88cf3ed1d4d283e20e0f282ff89eeae
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-10-12 17:46:40 +0200

    Update Spaces: cleaner code, better test

--- lagom/envs/spaces/__init__.py
+++ lagom/envs/spaces/__init__.py
@@ -1,8 +1,8 @@
 from .space import Space
 
+from .discrete import Discrete
 from .box import Box
 from .dict import Dict
-from .discrete import Discrete
-from .product import Product
+from .tuple import Tuple
 
 from .convert_gym_space import convert_gym_space
