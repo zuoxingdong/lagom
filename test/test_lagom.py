abd1c98b9b39b65b475abd21e97028e907a2cd40
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-10-12 14:45:56 +0200

    Add new test
    Former-commit-id: 438caeeaeffaa49cb9672a12f6d214a85dc510c8

--- test/test_lagom.py
+++ test/test_lagom.py
@@ -1,9 +1,12 @@
 import pytest
 
+import torch
 import numpy as np
 
 import os
+from pathlib import Path
 
+from lagom import BaseAlgorithm
 from lagom import Seeder
 from lagom import Logger
 from lagom import pickle_load
@@ -12,8 +15,25 @@ from lagom import yaml_load
 from lagom import yaml_dump
 
 
+class ToyAlgorithm(BaseAlgorithm):
+    def __call__(self, config, seed, device):
+        return config, seed, device
+
+
 class TestLagom(object):
-    def test_seeding(self):
+    def test_base_algorithm(self):
+        algo = ToyAlgorithm()
+        assert isinstance(algo, BaseAlgorithm)
+        
+        config, seed, device = algo(config={'lr': 0.1}, seed=1, device=torch.device('cpu'))
+        
+        assert isinstance(config, dict) and len(config) == 1
+        assert 'lr' in config and config['lr'] == 0.1
+        
+        assert seed == 1
+        assert device.type == 'cpu'
+    
+    def test_seeder(self):
         seeder = Seeder(init_seed=0)
         
         # Single list of seeds
@@ -22,14 +42,13 @@ class TestLagom(object):
         seeds = seeder(size=5)
         assert len(seeds) == 5
         
-        # Batch of seeds
+        # Batched seeds
         seeds = seeder(size=[1, 3])
         assert np.alltrue(np.array(seeds).shape == (1, 3))
         seeds = seeder(size=[2, 3])
         assert np.alltrue(np.array(seeds).shape == (2, 3))
         
     def test_pickle_yaml(self):
-        # Create some data
         a = {'one': 1, 'two': [2, 3]}
         b = {'three': 3, 'four': [4, 5]}
         c = [a, b]
@@ -46,62 +65,63 @@ class TestLagom(object):
         # Pickle
         pickle_dump(c, '.tmp_pickle')
         _check(pickle_load('.tmp_pickle.pkl'))
-        # remove the file
+        
         os.unlink('.tmp_pickle.pkl')
         
         # Yaml
         yaml_dump(c, '.tmp_yaml')
         _check(yaml_load('.tmp_yaml.yml'))
-        # remove the file
+        
         os.unlink('.tmp_yaml.yml')
     
     def test_logger(self):
-        logger = Logger(name='logger')
+        logger = Logger()
 
-        logger.log('iteration', 1)
-        logger.log('learning_rate', 1e-3)
-        logger.log('training_loss', 0.12)
-        logger.log('evaluation_loss', 0.14)
+        logger('iteration', 1)
+        logger('learning_rate', 1e-3)
+        logger('train_loss', 0.12)
+        logger('eval_loss', 0.14)
 
-        logger.log('iteration', 2)
-        logger.log('learning_rate', 5e-4)
-        logger.log('training_loss', 0.11)
-        logger.log('evaluation_loss', 0.13)
+        logger('iteration', 2)
+        logger('learning_rate', 5e-4)
+        logger('train_loss', 0.11)
+        logger('eval_loss', 0.13)
 
-        logger.log('iteration', 3)
-        logger.log('learning_rate', 1e-4)
-        logger.log('training_loss', 0.09)
-        logger.log('evaluation_loss', 0.10)
-        
-        # Test dump, because dump will call print, impossible to use assert
+        logger('iteration', 3)
+        logger('learning_rate', 1e-4)
+        logger('train_loss', 0.09)
+        logger('eval_loss', 0.10)
+
+        def check(logs):
+            assert len(logs) == 4
+            assert list(logs.keys()) == ['iteration', 'learning_rate', 'train_loss', 'eval_loss']
+            assert logs['iteration'] == [1, 2, 3]
+            assert np.allclose(logs['learning_rate'], [1e-3, 5e-4, 1e-4])
+            assert np.allclose(logs['train_loss'], [0.12, 0.11, 0.09])
+            assert np.allclose(logs['eval_loss'], [0.14, 0.13, 0.10])
+
+        check(logger.logs)
+
         logger.dump()
-        logger.dump(keys=None, index=None, indent=1)
-        logger.dump(keys=None, index=None, indent=2)
-        logger.dump(keys=['iteration', 'evaluation_loss'], index=None, indent=0)
-        logger.dump(keys=None, index=0, indent=0)
-        logger.dump(keys=None, index=2, indent=0)
-        logger.dump(keys=None, index=[0, 2], indent=0)
-        logger.dump(keys=['iteration', 'training_loss'], index=[0, 2], indent=0)
-        
-        # Test save function
-        file = './test_logger_file'
-        logger.save(file=file)
-        
-        assert os.path.exists(file)
-        
-        # Load file
-        logging = Logger.load(file)
-        
-        assert len(logging) == 4
-        assert 'iteration' in logging
-        assert 'learning_rate' in logging
-        assert 'training_loss' in logging
-        assert 'evaluation_loss' in logging
-        
-        assert np.allclose(logging['iteration'], [1, 2, 3])
-        assert np.allclose(logging['learning_rate'], [1e-3, 5e-4, 1e-4])
-        assert np.allclose(logging['training_loss'], [0.12, 0.11, 0.09])
-        assert np.allclose(logging['evaluation_loss'], [0.14, 0.13, 0.1])
-        
-        # Delete the temp logger file
-        os.unlink(file)
+        logger.dump(keys=['iteration'])
+        logger.dump(keys=['iteration', 'train_loss'])
+        logger.dump(index=0)
+        logger.dump(index=[1, 2])
+        logger.dump(index=0)
+        logger.dump(keys=['iteration', 'eval_loss'], index=1)
+        logger.dump(keys=['iteration', 'learning_rate'], indent=1)
+        logger.dump(keys=['iteration', 'train_loss'], index=[0, 2], indent=1)
+
+        f = Path('./logger_file')
+        logger.save(f)
+        f = f.with_suffix('.pkl')
+        assert f.exists()
+
+        logs = pickle_load(f)
+        check(logs)
+
+        f.unlink()
+        assert not f.exists()
+
+        logger.clear()
+        assert len(logger.logs) == 0
