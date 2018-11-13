d1494724e7d8be4cf6916f8da6fc1e9abb1ceac9
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-11-13 21:09:46 +0100

    New history objects, their runners and their metrics
    Former-commit-id: 0dc7ad3887512bd72fc78ef65ec0ac035c56dc82

--- lagom/history/metrics/gae.py
+++ lagom/history/metrics/gae.py
@@ -1,12 +1,14 @@
-from lagom.history import Segment
+from lagom.history import BatchEpisode
+from lagom.history import BatchSegment
 
 from lagom.transform import ExpFactorCumSum
 
-from .td import td0_error_from_trajectory
+from .td import td0_error_from_episode
+from .td import td0_error_from_segment
 
 
-def gae(list_td0_error, gamma, lam):
-    r"""Calculate a list of generalized advantage estimation (GAE) given a list of TD(0) errors and discounted factor. 
+def gae_from_episode(batch_episode, all_Vs, last_Vs, gamma, lam):
+    r"""Calculate the Generalized Advantage Estimation (GAE) of a batch of episodic transitions.
     
     Let :math:`\delta_t` be the TD(0) error at time step :math:`t`, the GAE at time step :math:`t` is calculated
     as follows
@@ -15,68 +17,51 @@ def gae(list_td0_error, gamma, lam):
         A_t^{\mathrm{GAE}(\gamma, \lambda)} = \sum_{k=0}^{\infty}(\gamma\lambda)^k \delta_{t + k}
     
     Args:
-        list_td0_error (list): a list of TD(0) errors. 
-        gamma (float): a discounted factor. 
+        batch_episode (BatchEpisode): a batch of episodic transitions. 
+        all_Vs (object): the value of states in all transitions. 
+        last_Vs (object): the value of the final states in the episode.
+        gamma (float): discounted factor.
         lam (float): GAE parameter. 
         
     Returns
     -------
-    list_gae : list
-        a list of GAE values.
+    out : object
+        GAE values
     """
-    assert isinstance(list_td0_error, list)
-    assert gamma >= 0.0 and gamma <= 1.0
-    assert lam >= 0.0 and lam <= 1.0
+    assert isinstance(batch_episode, BatchEpisode)
     
-    out = ExpFactorCumSum(gamma*lam)(list_td0_error)
+    td0_error = td0_error_from_episode(batch_episode, all_Vs, last_Vs, gamma)
+    f = ExpFactorCumSum(gamma*lam)
+    out = f(td0_error)
     
     return out
 
 
-def gae_from_trajectory(trajectory, Vs, V_last, gamma, lam):
-    r"""Return a list of GAE values from a trajectory. 
+def gae_from_segment(batch_segment, all_Vs, last_Vs, gamma, lam):
+    r"""Calculate the Generalized Advantage Estimation (GAE) of a batch of rolling segments.
+    
+    Let :math:`\delta_t` be the TD(0) error at time step :math:`t`, the GAE at time step :math:`t` is calculated
+    as follows
+    
+    .. math::
+        A_t^{\mathrm{GAE}(\gamma, \lambda)} = \sum_{k=0}^{\infty}(\gamma\lambda)^k \delta_{t + k}
     
     Args:
-        trajectory (Trajectory): a trajectory
-        Vs (list): a list of state values for each time step excluding the final state. 
-        V_last (object): the value of the final state in the trajectory
-        gamma (float): discounted factor
-        lam (float): GAE parameter
+        batch_segment (BatchSegment): a batch of rolling segments. 
+        all_Vs (object): the value of states in all transitions. 
+        last_Vs (object): the value of the final states in the episode.
+        gamma (float): discounted factor.
+        lam (float): GAE parameter. 
         
     Returns
     -------
-    out : list
-         a list of GAE values
+    out : object
+        GAE values
     """
-    list_td0_error = td0_error_from_trajectory(trajectory, Vs, V_last, gamma)
+    assert isinstance(batch_segment, BatchSegment)
     
-    out = gae(list_td0_error, gamma, lam)
+    td0_error = td0_error_from_segment(batch_segment, all_Vs, last_Vs, gamma)
+    f = ExpFactorCumSum(gamma*lam)
+    out = f(td0_error, mask=batch_segment.numpy_masks)
     
-    return out
-
-
-def gae_from_segment(segment, all_Vs, all_V_last, gamma, lam):
-    r"""Return a list of GAE values from a segment. 
-    
-    Args:
-        segment (Segment): a segment
-        all_Vs (list): a list of state values for each time step and each inner trajectory
-            excluding the final state. 
-        all_V_last (list): a list of final state values for each inner trajectory. 
-        gamma (float): discounted factor
-        lam (float): GAE parameter
-        
-    Returns
-    -------
-    out : list
-         a list of GAE values
-    """
-    assert isinstance(segment, Segment)
-    assert isinstance(all_Vs, list) and len(all_Vs) == len(segment.trajectories)
-    assert len(all_V_last) == len(segment.trajectories)
-    
-    out = []
-    for trajectory, Vs, V_last in zip(segment.trajectories, all_Vs, all_V_last):
-        out += gae_from_trajectory(trajectory, Vs, V_last, gamma, lam)
-        
     return out
