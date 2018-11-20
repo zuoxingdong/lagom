286122589b8c3a0672c917ab94d169856c50a782
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-11-20 17:39:18 +0100

    Fix test
    Former-commit-id: c99b91100883d66917a4168bd2e360e6cb6f1f3d

--- test/test_testutils.py
+++ test/test_testutils.py
@@ -18,19 +18,19 @@ def test_sanity_check_env():
     assert all(len(i) == 0 for i in info)
 
     obs, reward, done, info = env.step([1, 0])
-    assert np.allclose(obs, [[2.01], [2.01]])
+    assert np.allclose(obs, [[0.01], [2.01]])
     assert np.allclose(reward, [0.2, 0.2])
     assert np.allclose(done, [True, False])
-    assert info[0]['init_observation'] == [0.01] and len(info[1]) == 0
+    assert info[0]['terminal_observation'] == [2.01] and len(info[1]) == 0
 
     obs, reward, done, info = env.step([1, 1])
-    assert np.allclose(obs, [[1.01], [3.01]])
+    assert np.allclose(obs, [[1.01], [0.01]])
     assert np.allclose(reward, [0.1, 0.3])
     assert np.allclose(done, [False, True])
-    assert len(info[0]) == 0 and info[1]['init_observation'] == [0.01]
+    assert len(info[0]) == 0 and info[1]['terminal_observation'] == [3.01]
 
     obs, reward, done, info = env.step([0, 0])
-    assert np.allclose(obs, [[2.01], [1.01]])
+    assert np.allclose(obs, [[0.01], [1.01]])
     assert np.allclose(reward, [0.2, 0.1])
     assert np.allclose(done, [True, False])
-    assert info[0]['init_observation'] == [0.01] and len(info[1]) == 0
+    assert info[0]['terminal_observation'] == [2.01] and len(info[1]) == 0
