2a28b8ccea7d3346c20143e5581e283577ae1abf
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-11-23 17:08:55 +0100

    Update envs
    * Add TimeAwareObservation wrapper
    
    * Add running que in VecMonitor: useful for running average of returns
    
    
    Former-commit-id: aeb98fc9afe56f2ac2a23ad2fe484798c34d00ab [formerly d19c548b29306e5a622bd740138693784c2ef3dd]
    Former-commit-id: 73d1444f8459ffc033eaccfb387136f8d5caee3a

--- lagom/envs/vec_env/vec_monitor.py
+++ lagom/envs/vec_env/vec_monitor.py
@@ -1,4 +1,5 @@
 from time import time
+from collections import deque
 
 import numpy as np
 
@@ -6,26 +7,32 @@ from .vec_env import VecEnvWrapper
 
 
 class VecMonitor(VecEnvWrapper):
-    r"""Record episode reward, length and time and report it when an episode terminates. """
-    def __init__(self, venv):
+    r"""Record episode reward, horizon and time and report it when an episode terminates. """
+    def __init__(self, venv, deque_size=100):
         super().__init__(venv)
         
         self.t0 = time()
         self.episode_rewards = np.zeros(self.N, dtype=np.float32)
-        self.episode_lengths = np.zeros(self.N, dtype=np.int32)
+        self.episode_horizons = np.zeros(self.N, dtype=np.int32)
+        
+        self.return_queue = deque(maxlen=deque_size)
+        self.horizon_queue = deque(maxlen=deque_size)
         
     def step_wait(self):
         observations, rewards, dones, infos = self.venv.step_wait()
         
         self.episode_rewards += rewards
-        self.episode_lengths += 1
+        self.episode_horizons += 1
         for i, done in enumerate(dones):
             if done:
                 infos[i]['episode'] = {'return': self.episode_rewards[i], 
-                                       'horizon': self.episode_lengths[i],
+                                       'horizon': self.episode_horizons[i],
                                        'time': round(time() - self.t0, 4)}
+                self.return_queue.append(self.episode_rewards[i])
+                self.horizon_queue.append(self.episode_horizons[i])
+                
                 self.episode_rewards[i] = 0.0
-                self.episode_lengths[i] = 0
+                self.episode_horizons[i] = 0
         
         return observations, rewards, dones, infos
         
@@ -33,7 +40,7 @@ class VecMonitor(VecEnvWrapper):
         observations = self.venv.reset()
         
         self.episode_rewards.fill(0.0)
-        self.episode_lengths.fill(0)
+        self.episode_horizons.fill(0)
         
         return observations
         
