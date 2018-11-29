29bfef04654e963e797fb618f3dfda59785107fe
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-11-29 13:51:42 +0100

    Remove transforms: clip, centralize, normalize, standardize
    Former-commit-id: 3850643c995185ba987637d9c86b0bf0ab07cc32 [formerly 792b625060cd83da067302bc86244fe6ab913b6c]
    Former-commit-id: b0ec0e32a5c59d03e528477e2ccbf5d923394104

--- lagom/transform/__init__.py
+++ lagom/transform/__init__.py
@@ -1,13 +1,9 @@
 from .base_transform import BaseTransform
 
-from .centralize import Centralize
-from .clip import Clip
 from .exp_factor_cumsum import ExpFactorCumSum
 from .explained_variance import ExplainedVariance
 from .interp_curve import InterpCurve
 from .linear_schedule import LinearSchedule
-from .normalize import Normalize
 from .rank_transform import RankTransform
 from .running_mean_std import RunningMeanStd
 from .smooth_filter import SmoothFilter
-from .standardize import Standardize
--- test/test_transform.py
+++ test/test_transform.py
@@ -2,17 +2,13 @@ import numpy as np
 
 import pytest
 
-from lagom.transform import Centralize
-from lagom.transform import Clip
 from lagom.transform import ExpFactorCumSum
 from lagom.transform import ExplainedVariance
 from lagom.transform import InterpCurve
 from lagom.transform import LinearSchedule
-from lagom.transform import Normalize
 from lagom.transform import RankTransform
 from lagom.transform import RunningMeanStd
 from lagom.transform import SmoothFilter
-from lagom.transform import Standardize
 
 
 def check_dtype(out):
@@ -20,63 +16,6 @@ def check_dtype(out):
         assert isinstance(out, np.float32)
     else:
         assert out.dtype == np.float32
-    
-
-def test_centralize():
-    centralize = Centralize()
-    
-    with pytest.raises(AssertionError):
-        centralize(1.2, 0)
-
-    out = centralize([1, 2, 3, 4], 0)
-    assert np.allclose(out, [-1.5, -0.5, 0.5, 1.5])
-    assert out.mean() == 0.0
-    check_dtype(out)
-
-    out = centralize([[1, 3], [2, 4], [3, 5]], 0)
-    assert np.allclose(out, [[-1, -1], [0, 0], [1, 1]])
-    assert np.allclose(out.mean(0), 0.0)
-    check_dtype(out)
-
-    out = centralize([[1, 3], [2, 4], [3, 5]], 1)
-    assert np.allclose(out, [[-1, 1], [-1, 1], [-1, 1]])
-    assert np.allclose(out.mean(1), 0.0)
-    check_dtype(out)
-
-    mean = [0.1, 0.2, 0.3]
-    out = centralize([1, 2, 3], 0, mean=mean)
-    assert np.allclose(out, [0.9, 1.8, 2.7])
-    check_dtype(out)
-
-    mean = [0.1, 0.2]
-    out = centralize([[1, 3], [2, 4], [3, 5]], 0, mean=mean)
-    assert np.allclose(out, [[0.9, 2.8], [1.9, 3.8], [2.9, 4.8]])
-    check_dtype(out)
-    
-    
-def test_clip():
-    clip = Clip()
-
-    out = clip(1.2, 0.5, 1.5)
-    assert np.isscalar(out) and np.allclose(out, 1.2)
-    check_dtype(out)
-
-    out = clip(0.2, 0.5, 1.5)
-    assert np.isscalar(out) and np.allclose(out, 0.5)
-    check_dtype(out)
-
-    out = clip(2.3, 0.5, 1.5)
-    assert np.isscalar(out) and np.allclose(out, 1.5)
-    check_dtype(out)
-
-    out = clip([1, 2, 3], 1.5, 2.5)
-    assert out.shape == (3,) and np.allclose(out, [1.5, 2, 2.5])
-    check_dtype(out)
-
-    out = clip([1, 2, 3], [0, 1, 3.5], [0.5, 2, 9])
-    assert out.shape == (3,) and np.allclose(out, [0.5, 2, 3.5])
-    check_dtype(out)
-    
     
 def test_expfactorcumsum():
     f = ExpFactorCumSum(0.1)
@@ -191,33 +130,6 @@ def test_linear_schedule():
     assert scheduler(5) == 0.1
     assert all([scheduler(i) == 0.1 for i in [6, 7, 8]])
     
-    
-def test_normalize():
-    normalize = Normalize(eps=1.1920929e-07)
-
-    with pytest.raises(AssertionError):
-        normalize(-1, 0)
-
-    out = normalize([1, 2, 3, 4], 0)
-    assert np.allclose(out, [0., 0.33333334, 0.6666667, 1.])
-    check_dtype(out)
-
-    out = normalize([1, 2, 3, 4], 0, minimal=0, maximal=1)
-    assert np.allclose(out, [0.9999999, 1.9999998, 2.9999998, 3.9999995])
-    check_dtype(out)
-
-    out = normalize([[1, 5, 8], [2, 4, 6]], 0)
-    assert np.allclose(out, [[0, 0.999999, 1], [0.999999, 0, 0]])
-    check_dtype(out)
-
-    out = normalize([[1, 5, 8], [2, 4, 6]], 1)
-    assert np.allclose(out, [[0, 0.5714286, 1], [0, 0.5, 1]])
-    check_dtype(out)
-
-    out = normalize([1, 2, 3, 4], 0, minimal=1, maximal=3)
-    assert np.allclose(out, [0., 0.49999994, 0.9999999, 1.4999999]) 
-    check_dtype(out)
-    
 
 def test_rank_transform():
     f = RankTransform()
@@ -283,46 +195,3 @@ def test_smooth_filter():
     out = f(y, window_length=31, polyorder=10)
     assert out.shape == (100,)
     assert out.dtype == np.float32
-
-      
-def test_standardize():
-    standardize = Standardize(eps=1.1920929e-07)
-
-    with pytest.raises(AssertionError):
-        standardize(1.2, 0)
-        
-    out = standardize([1, 2, 3, 4], 0)
-    assert np.allclose(out, [-1.3416406, -0.44721353, 0.44721353, 1.3416406])
-    assert np.isclose(out.mean(), 0.0)
-    assert np.isclose(out.std(), 1.0)
-    check_dtype(out)
-    
-    out = standardize([1, 2, 3, 4], 0, mean=1, std=2)
-    assert np.allclose(out, [0, 0.5, 1, 1.5])
-    check_dtype(out)
-
-    out = standardize([[5, 3, 12], [-40, 2, 16], [17, 8, -2]], 0)
-    assert np.allclose(out, [[0.44832653, -0.5080005, 0.43193418],
-                             [-1.3857366, -0.88900083, 0.9502552],
-                             [0.93741, 1.3970011, -1.3821895]])
-    assert np.allclose(out.mean(0), 0.0, atol=1e-7)
-    assert not np.allclose(out.mean(1), 0.0, atol=1e-7)
-    assert np.allclose(out.std(0), 1.0)
-    assert not np.allclose(out.std(1), 1.0)
-    check_dtype(out)
-        
-    out = standardize([[5, 3, 12], [-40, 2, 16], [17, 8, -2]], 1)
-    assert np.allclose(out, [[-0.43193415, -0.95025516, 1.3821894],
-                             [-1.372813, 0.39223227, 0.9805807],
-                             [1.2027031, 0.0429537, -1.2456566]])
-    assert np.allclose(out.mean(1), 0.0, atol=1e-7)
-    assert not np.allclose(out.mean(0), 0.0, atol=1e-7)
-    assert np.allclose(out.std(1), 1.0)
-    assert not np.allclose(out.std(0), 1.0)
-    check_dtype(out)
-
-    out = standardize([[5, 3, 12], [-40, 2, 16], [17, 8, -2]], 0, mean=[1, 2, 3], std=[4, 5, 6])
-    assert np.allclose(out, [[1., 0.2, 1.5],
-                             [-10.25, 0., 2.1666667],
-                             [4., 1.2, -0.8333333]])
-    check_dtype(out)
