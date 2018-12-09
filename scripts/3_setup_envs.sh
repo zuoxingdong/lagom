e0af43e9f337f6bfa0ea25226e73fb9a59b247b6
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-12-09 16:52:29 +0100

    Update scripts
    Former-commit-id: f249d72ed0ad4ee91b82e6ba21356cd9cfa90169

--- scripts/3_setup_envs.sh
+++ scripts/3_setup_envs.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-#############################
-# Install dependencies      #
-# Date: 2018-10-11  	    #
-# Author: Xingdong Zuo      #
-#############################
+################################################
+# Configure environment & install dependencies #
+# Date: 2018-12-09  	                       #
+# Author: Xingdong Zuo                         #
+################################################
 
 # Stop when some intermediate command is failed
 set -e
-
-# Alias of conda command
-export CONDA=$HOME/anaconda3/bin/conda
 
 # Set up .vimrc for tab as 4 spaces
 echo "# Appended for Vim"
 echo "set tabstop=4" >> ~/.vimrc
 echo "set shiftwidth=4" >> ~/.vimrc
 echo "set expandtab" >> ~/.vimrc
+echo "" >> ~/.vimrc
 
 # Install dependencies
-$CONDA update --all
-$CONDA install -y ipython patchelf
-
 pip install --upgrade pip
-pip install -r ../requirements.txt
+pip install -q -r ../requirements.txt
