0f89642a83466ca7a4a829446804f5fa4dff747e
Xingdong Zuo <zuoxingdong@hotmail.com>
2018-12-11 17:08:36 +0100

    Fix CI
    Former-commit-id: 794c9ad484981f334f76197a6eb0e1eabd489ed1

--- scripts/1_setup_system.sh
+++ scripts/1_setup_system.sh
@@ -2,7 +2,7 @@
 
 ################################
 # Install system-wide packages #
-# Date: 2018-12-09  	       #
+# Date: 2018-12-11  	       #
 # Author: Xingdong Zuo         #
 ################################
 
@@ -10,6 +10,7 @@
 set -e
 
 # Show Ubuntu version
+sudo apt install -y lsb-release
 echo '#######################################'
 echo '# Your current Ubuntu OS information: # '
 echo '#######################################'
@@ -18,25 +19,27 @@ lsb_release -a
 # Update package list
 sudo apt update
 # Upgrade available packages
-sudo apt upgrade
-sudo apt dist-upgrade
+sudo apt -y upgrade
+sudo apt -y dist-upgrade
 # Install some packages
 # libopenmpi-dev: support mpi4py
+# cmake: for C
 # zlib1g-dev: for compression e.g. gzip
-# libjpeg-dev: JPEG support
-# xvfb: Fake screen, useful for gym rendering on the server
-# libav-tools: Audio and Video processing
-# xorg-dev: X.org Window system
-# python3-opengl: Python 3 OpenGL
 # libboost-all-dev: Boost C++ library
-# libsdl2-dev: About video processing
+# python3-dev: Python 3 library
+# libjpeg-dev: JPEG support
+# ffmpeg: Audio and Video processing
+# python3-opengl: Python 3 OpenGL
+# python-pyglet: pyglet
+# libsdl2-dev: video processing
 # swig: Useful for Box2D in gym
 # libglew-dev: OpenGL stuff, useful for mujoco-py to work. 
-# libglu1-mesa: for GLU
-# libglu1-mesa-dev: for GLU
-# libgl1-mesa-dev: for GL
+# libosmesa6-dev: for Mujoco
+# patchelf: ELF executables
+# xorg-dev: X.org Window system
 # libxinerama1: for X Window System
 # libxcursor1: for X cursor
+# xvfb: Fake screen, useful for gym rendering on the server
 # htop: CPU monitoring
 # vim: Vim editor
-sudo apt install libopenmpi-dev zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python3-opengl libboost-all-dev libsdl2-dev swig libglew-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-dev libxinerama1 libxcursor1 htop vim
+sudo apt install -y libopenmpi-dev cmake zlib1g-dev libboost-all-dev python3-dev libjpeg-dev ffmpeg python3-opengl python-pyglet libsdl2-dev swig libglew-dev libosmesa6-dev patchelf xorg-dev libxinerama1 libxcursor1 xvfb htop vim
