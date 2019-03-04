003fd72ca7975ec5553551ab4e24b98aff044e46
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-03-04 13:53:33 +0100

    Update examples
    Former-commit-id: 83f9c402bb2c97fa95ec049472fd850333dd427f [formerly 93f07c237cf6b38c1b9206efb807fd04ac08b47f]
    Former-commit-id: 22ed9ee4d5bc34c2e0d430ae80d25c36ea3d5be4

--- examples/reinforcement_learning/ppo/dataset.py
+++ examples/reinforcement_learning/ppo/dataset.py
@@ -1,42 +1,33 @@
 from torch.utils import data
 
-from lagom.history.metrics import final_state_from_segment
-from lagom.history.metrics import gae_from_segment
-
 
 class Dataset(data.Dataset):
-    def __init__(self, env_spec, observations, actions, logprobs, entropies, all_Vs, Qs, As):
-        self.env_spec = env_spec
-        # eliminate the last observation, to have consistent shape with others
-        self.observations = observations[:, :-1, ...]
-        self.actions = actions
+    def __init__(self, D, logprobs, entropies, Vs, Qs, As):
+        self.D = D
+        self.observations = D.batch_observations
+        self.actions = D.batch_actions
         self.logprobs = logprobs.detach().cpu().numpy()
         self.entropies = entropies.detach().cpu().numpy()
-        self.all_Vs = all_Vs.detach().cpu().numpy()
+        self.Vs = Vs.detach().cpu().numpy()
         self.Qs = Qs.detach().cpu().numpy()
         self.As = As.detach().cpu().numpy()
         
-        N, T = self.logprobs.shape
-        self.total_N = N*T
-        
         # rolling batch [N, T, ...] to [N*T, ...]
-        obs_shape = self.env_spec.observation_space.shape
-        action_shape = self.env_spec.action_space.shape
-        self.observations = self.observations.reshape([self.total_N, *obs_shape])
-        self.actions = self.actions.reshape([self.total_N, *action_shape])
+        self.observations = self.observations.reshape([len(self), *D.env.observation_space.shape])
+        self.actions = self.actions.reshape([len(self), *D.env.action_space.shape])
         self.logprobs = self.logprobs.flatten()
         self.entropies = self.entropies.flatten()
-        self.all_Vs = self.all_Vs.flatten()
+        self.Vs = self.Vs.flatten()
         self.Qs = self.Qs.flatten()
         self.As = self.As.flatten()
         
-        assert self.observations.shape[0] == self.total_N
-        assert self.actions.shape[0] == self.total_N
-        assert all([item.shape == (self.total_N,) for item in [self.logprobs, self.entropies, 
-                                                               self.all_Vs, self.Qs, self.As]])
-        
+        assert self.observations.shape[0] == len(self)
+        assert self.actions.shape[0] == len(self)
+        assert all([item.shape == (len(self),) for item in [self.logprobs, self.entropies, 
+                                                            self.Vs, self.Qs, self.As]])
+
     def __len__(self):
-        return self.total_N
+        return self.D.N*self.D.T
         
     def __getitem__(self, i):
-        return self.observations[i], self.actions[i], self.logprobs[i], self.entropies[i], self.all_Vs[i], self.Qs[i], self.As[i]
+        return self.observations[i], self.actions[i], self.logprobs[i], self.entropies[i], self.Vs[i], self.Qs[i], self.As[i]
