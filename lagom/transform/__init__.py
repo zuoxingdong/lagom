b954dfe66e9fe2f7dbcd389e248e82376d48f4ed
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-03-23 20:00:09 +0100

    Rename RunningAverage to PolyakAverage
    Former-commit-id: f5fcc6e5c6594ce978a67d27c7b08e65bb98fc8e [formerly 0575e257b67b81ac0044f22f9503ee8c4b404ec3]
    Former-commit-id: 7595e8b23046bc574ba0b353b2106c53960919ca

--- lagom/transform/__init__.py
+++ lagom/transform/__init__.py
@@ -3,6 +3,6 @@ from .geometric_cumsum import geometric_cumsum
 from .explained_variance import explained_variance
 from .linear_schedule import LinearSchedule
 from .rank_transform import rank_transform
-from .running_average import RunningAverage
+from .polyak_average import PolyakAverage
 from .running_mean_std import RunningMeanStd
 from .smooth_filter import smooth_filter
