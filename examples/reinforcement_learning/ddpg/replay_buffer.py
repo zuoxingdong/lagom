8340f8405c6891fee23fdc40910241b3b0ae0902
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-04-02 16:48:47 +0200

    update VPG/PPO/DDPG
    Former-commit-id: dc1975a033ab2588a1c3bc0f6fe53fd6a61c5667 [formerly b1d51cfc4eeb9c9cf29c04c897e72c3926311d67]
    Former-commit-id: d6121e25e9c918ecad612c0b1803a0a839638ade

--- examples/reinforcement_learning/ddpg/replay_buffer.py
+++ examples/reinforcement_learning/ddpg/replay_buffer.py
@@ -2,7 +2,6 @@ from collections import deque
 
 import random
 import numpy as np
-
 import torch
 
 
@@ -28,34 +27,14 @@ class ReplayBuffer(object):
         return len(self.buffer)
 
     def add(self, observation, action, reward, next_observation, done):
-        # Difference with DQN replay buffer: we handle raw observation (no pixel) for continuous control
         transition = (observation, action, reward, next_observation, done)
         self.buffer.append(transition)
         
     def sample(self, batch_size):
-        r"""Sample a batch of experiences.
-        
-        Args:
-            batch_size (int): the number of transitions to sample
-            
-        Returns
-        -------
-        observations : ndarray
-            batch of observations
-        actions : ndarray
-            batch of actions
-        rewards : ndarray
-            batch of rewards
-        next_observations : ndarray
-            batch of next observations
-        masks : ndarray
-            batch of masks
-        
-        """
         D = random.sample(self.buffer, batch_size)  # unique samples, rather than random.choices
         D = zip(*D)
         observations, actions, rewards, next_observations, dones = list(map(lambda x: np.asarray(x), D))
         masks = 1. - dones
         D = (observations, actions, rewards, next_observations, masks)
-        D = list(map(lambda x: torch.from_numpy(x.astype(np.float32)).to(self.device), D))
+        D = list(map(lambda x: torch.from_numpy(x).float().to(self.device), D))
         return D
