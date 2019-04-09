96d95f3656df6c7dd8a9446fdf2f6af727197651
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-04-09 10:36:11 +0200

    Update vis: remove `lineplot`, add `set_ticker`
    Former-commit-id: 777bfdf351b5847f7779e353bfa620c65f054f61 [formerly c7f684bc533a6076e23cd3d2c7aadea62592005b]
    Former-commit-id: a90f6d2d4420bdf3c87c77c7a9f43865e3b79bfc

--- lagom/vis/__init__.py
+++ lagom/vis/__init__.py
@@ -1,11 +1,9 @@
-from .curve_plot import lineplot
-from .curve_plot import auto_ax
-from .curve_plot import tick_formatter
-
-from .grid_image import GridImage
-
 try:  # workaround on server without fake screen but still running other things well
     from .image_viewer import ImageViewer
 except ImportError:
     import warnings
     warnings.warn('ImageViewer failed to import due to pyglet. ')
+
+from .grid_image import GridImage
+
+from .utils import set_ticker
