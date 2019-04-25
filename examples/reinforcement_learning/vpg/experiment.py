73ca4500658373579a2e93eecaf4fcd177b11f8d
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-04-25 10:59:41 +0200

    update VPG

--- examples/reinforcement_learning/vpg/experiment.py
+++ examples/reinforcement_learning/vpg/experiment.py
@@ -25,14 +25,7 @@ from engine import Engine
 
 config = Config(
     {'cuda': True, 
-     
-     ######
-     'cuda_ids': [1, 2, 3, 5, 6, 7],
-     ######
-     
-     
-     
-     'log.dir': 'logs/default_new', 
+     'log.dir': 'logs/default', 
      'log.freq': 10, 
      'checkpoint.freq': 50,
      
