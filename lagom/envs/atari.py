f4446b6d61b9629d103c4ef48646dfee2946660c
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-04-26 17:19:56 +0200

    update Atari wrapper: more efficient
    Former-commit-id: de42a874eb0d912faed55049de920e8198ab0c47

--- lagom/envs/atari.py
+++ lagom/envs/atari.py
@@ -1,24 +1,23 @@
 import numpy as np
 
 import gym
-from gym import Wrapper
+from gym.spaces import Box
 
-from .wrappers import ResizeObservation
-from .wrappers import GrayScaleObservation
+import cv2
+
 from .wrappers import FrameStack
-
 from .wrappers import TimeLimit
 
 
-class AtariPreprocessing(Wrapper):
+class AtariPreprocessing(gym.Wrapper):
     r"""Atari 2600 preprocessings. 
-    
+
     This class follows the guidelines in 
     Machado et al. (2018), "Revisiting the Arcade Learning Environment: 
     Evaluation Protocols and Open Problems for General Agents".
 
     Specifically:
-    
+
     * NoopReset: obtain initial state by taking random number of no-ops on reset. 
     * FireReset: take action on reset for environments that are fixed until firing. 
     * Frame skipping: 4 by default
@@ -26,88 +25,86 @@ class AtariPreprocessing(Wrapper):
     * Termination signal when a life is lost: turned off by default. Not recommended by Machado et al. (2018).
     * Resize to a square image: 84x84 by default
     * Grayscale
-    
+
     Args:
         env (Env): environment
         noop_max (int): max number of no-ops
         frame_skip (int): the frequency at which the agent experiences the game. 
-        done_on_life_loss (bool): if True, then step() returns done=True whenever a
+        screen_size (int): resize Atari frame
+        terminal_on_life_loss (bool): if True, then step() returns done=True whenever a
             life is lost. 
-    
+
     """
-    def __init__(self, env, noop_max=30, frame_skip=4, done_on_life_loss=False):
+    def __init__(self, env, noop_max=30, frame_skip=4, screen_size=84, terminal_on_life_loss=False):
         super().__init__(env)
         assert frame_skip > 0
-        
+        assert screen_size > 0
+
         self.noop_max = noop_max
         assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
-        
+
         self.frame_skip = frame_skip
-        self.done_on_life_loss = done_on_life_loss
-        
+        self.screen_size = screen_size
+        self.terminal_on_life_loss = terminal_on_life_loss
+
         # buffer of most recent two observations for max pooling
-        self.obs_buffer = [np.empty(self.observation_space.shape, dtype=np.uint8),
-                           np.empty(self.observation_space.shape, dtype=np.uint8)]
-        
-        self.ale = self._get_ale(self.env)
+        self.obs_buffer = [np.empty(env.observation_space.shape[:2], dtype=np.uint8),
+                           np.empty(env.observation_space.shape[:2], dtype=np.uint8)]
+
+        self.ale = env.unwrapped.ale
         self.lives = 0
-        self.real_done = False
-        
-    def _get_ale(self, env):
-        while True:
-            if isinstance(env, gym.envs.atari.AtariEnv):
-                return env.ale
-            else:
-                env = env.unwrapped
-                
+        self.game_over = False
+
+        self.observation_space = Box(low=0, high=255, shape=(screen_size, screen_size), dtype=np.uint8)
+
     def step(self, action):
         R = 0.0
-        
+
         for t in range(self.frame_skip):
-            observation, reward, done, info = self.env.step(action)
+            _, reward, done, info = self.env.step(action)
             R += reward
-            self.real_done = done
-            
-            if self.done_on_life_loss:
+            self.game_over = done
+
+            if self.terminal_on_life_loss:
                 new_lives = self.ale.lives()
                 done = done or new_lives < self.lives
                 self.lives = new_lives
-                
+
             if done:
-                break
+                break    
             if t == self.frame_skip - 2:
-                self.obs_buffer[0] = observation
+                self.ale.GrayScaleObservation(self.obs_buffer[0])
             elif t == self.frame_skip - 1:
-                self.obs_buffer[1] = observation
-                
-        if self.frame_skip > 1:  # pooling: more efficieny in-place
-            np.maximum(self.obs_buffer[0], self.obs_buffer[1], out=self.obs_buffer[0])
-            
-        return self.obs_buffer[0], R, done, info
-                
+                self.ale.GrayScaleObservation(self.obs_buffer[1])    
+        return self._get_obs(), R, done, info
+
     def reset(self, **kwargs):
         # NoopReset
         self.env.reset(**kwargs)
         noops = self.env.unwrapped.np_random.randint(1, self.noop_max + 1)
         assert noops > 0
         for _ in range(noops):
-            obs, _, done, _ = self.env.step(0)
+            _, _, done, _ = self.env.step(0)
             if done:
-                obs = self.env.reset(**kwargs)
-        
+                self.env.reset(**kwargs)
+
         # FireReset
         action_meanings = self.env.unwrapped.get_action_meanings()
         if action_meanings[1] == 'FIRE' and len(action_meanings) >= 3:
-            obs, _, done, _ = self.env.step(1)
-            obs, _, done, _ = self.env.step(2)
-                
-        self.obs_buffer[0] = obs
-        self.obs_buffer[1].fill(0)
+            self.env.step(1)
+            self.env.step(2)
+
         self.lives = self.ale.lives()
-        if self.frame_skip > 1:  # pooling: more efficieny in-place
+        self.ale.getScreenGrayscale(self.obs_buffer[0])
+        self.obs_buffer[1].fill(0)
+        return self._get_obs()
+
+    def _get_obs(self):
+        if self.frame_skip > 1:  # more efficient in-place pooling
             np.maximum(self.obs_buffer[0], self.obs_buffer[1], out=self.obs_buffer[0])
-            
-        return self.obs_buffer[0]
+        obs = cv2.resize(self.obs_buffer[0], (self.screen_size, self.screen_size), interpolation=cv2.INTER_AREA)
+        obs = np.asarray(obs, dtype=np.uint8)
+        return obs
 
 
 def make_atari(name, sticky_action=True, max_episode_steps=None, lz4_compress=False):
@@ -135,8 +132,6 @@ def make_atari(name, sticky_action=True, max_episode_steps=None, lz4_compress=Fa
         env = TimeLimit(env, max_episode_steps)
     else:
         env = TimeLimit(env, env.spec.max_episode_steps)
-    env = ResizeObservation(env, 84)
-    env = GrayScaleObservation(env, keep_dim=False)
     env = AtariPreprocessing(env)
     env = FrameStack(env, 4, lz4_compress)    
     return env
