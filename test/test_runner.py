e7f782da5dce73e13e40d857ad4298ee399a44d6
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-05-07 13:31:56 +0200

    update runner: adapt to VecStepInfo
    Former-commit-id: 24ea4b230cae9017cc0e09317e5d98a057698b86

--- test/test_runner.py
+++ test/test_runner.py
@@ -10,6 +10,7 @@ import gym
 from lagom import RandomAgent
 from lagom.envs import make_vec_env
 from lagom.envs.wrappers import TimeLimit
+from lagom.envs.wrappers import VecStepInfo
 from lagom.runner import Trajectory
 from lagom.runner import EpisodeRunner
 
@@ -21,27 +22,27 @@ from .sanity_env import SanityEnv
 def test_trajectory(init_seed, T):
     make_env = lambda: TimeLimit(SanityEnv())
     env = make_vec_env(make_env, 1, init_seed)  # single environment
+    env = VecStepInfo(env)
     D = Trajectory()
     assert len(D) == 0
     assert not D.completed
     
-    observation = env.reset()
+    observation, _ = env.reset()
     D.add_observation(observation)
     for t in range(T):
         action = [env.action_space.sample()]
-        next_observation, reward, done, info = env.step(action)
-        # unbatched for [reward, done, info]
-        reward, done, info = map(lambda x: x[0], [reward, done, info])
-        if done:
-            D.add_observation([info['last_observation']])
+        next_observation, reward, step_info = env.step(action)
+        # unbatched for [reward, step_info]
+        reward, step_info = map(lambda x: x[0], [reward, step_info])
+        if step_info.last:
+            D.add_observation([step_info['last_observation']])
         else:
             D.add_observation(next_observation)
         D.add_action(action)
         D.add_reward(reward)
-        D.add_info(info)
-        D.add_done(done)
+        D.add_step_info(step_info)
         observation = next_observation
-        if done:
+        if step_info.last:
             with pytest.raises(AssertionError):
                 D.add_observation(observation)
             break
@@ -53,19 +54,20 @@ def test_trajectory(init_seed, T):
     assert len(D) == len(D.numpy_actions)
     assert len(D) == len(D.rewards)
     assert len(D) == len(D.numpy_rewards)
-    assert len(D) == len(D.dones)
     assert len(D) == len(D.numpy_dones)
     assert len(D) == len(D.numpy_masks)
     assert np.allclose(np.logical_not(D.numpy_dones), D.numpy_masks)
-    assert len(D) == len(D.infos)
+    assert len(D) == len(D.step_infos)
     if len(D) < T:
-        assert done
+        assert step_info.last
         assert D.completed
-        assert D.dones[-1]
-        assert np.allclose(D.observations[-1], [info['last_observation']])
-    if not done:
+        assert D.reach_terminal
+        assert not D.reach_time_limit
+        assert np.allclose(D.observations[-1], [step_info['last_observation']])
+    if not step_info.last:
         assert not D.completed
-        assert not D.dones[-1]
+        assert not D.reach_terminal
+        assert not D.reach_time_limit
 
 
 @pytest.mark.parametrize('env_id', ['Sanity', 'CartPole-v1', 'Pendulum-v0', 'Pong-v0'])
@@ -78,6 +80,7 @@ def test_episode_runner(env_id, num_env, init_seed, T):
     else:
         make_env = lambda: gym.make(env_id)
     env = make_vec_env(make_env, num_env, init_seed)
+    env = VecStepInfo(env)
     agent = RandomAgent(None, env, None)
     runner = EpisodeRunner()
     
@@ -85,6 +88,8 @@ def test_episode_runner(env_id, num_env, init_seed, T):
         with pytest.raises(AssertionError):
             D = runner(agent, env, T)
     else:
+        with pytest.raises(AssertionError):
+            runner(agent, env.env, T)  # must be VecStepInfo
         D = runner(agent, env, T)
         for traj in D:
             assert isinstance(traj, Trajectory)
@@ -97,6 +102,6 @@ def test_episode_runner(env_id, num_env, init_seed, T):
             assert traj.numpy_rewards.shape == (len(traj),)
             assert traj.numpy_dones.shape == (len(traj), )
             assert traj.numpy_masks.shape == (len(traj), )
-            assert len(traj.infos) == len(traj)
+            assert len(traj.step_infos) == len(traj)
             if traj.completed:
-                assert np.allclose(traj.observations[-1], traj.infos[-1]['last_observation'])
+                assert np.allclose(traj.observations[-1], traj.step_infos[-1]['last_observation'])
