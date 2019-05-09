55ef9751adf075b7c535173fe78277df7144e12a
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-05-09 16:13:17 +0200

    Update transform: add SegmentTree/SumTree/MinTree
    Former-commit-id: bb44b9be5727b46fce7b60bbc79ec620eea4cd9e [formerly b7278d52cb0b4549e0d0eaaccfa2f8f52450b845]
    Former-commit-id: dba1b16d73df36f40c5a2142a9b2cb363875f99b

--- lagom/transform/__init__.py
+++ lagom/transform/__init__.py
@@ -7,4 +7,7 @@ from .linear_schedule import LinearSchedule
 from .rank_transform import rank_transform
 from .polyak_average import PolyakAverage
 from .running_mean_var import RunningMeanVar
+from .segment_tree import SegmentTree
+from .segment_tree import SumTree
+from .segment_tree import MinTree
 from .smooth_filter import smooth_filter
