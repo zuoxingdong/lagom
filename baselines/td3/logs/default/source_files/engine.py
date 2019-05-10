7444f11c9e744215d95f4723a8cb32493eb5fc92
Xingdong Zuo <zuoxingdong@hotmail.com>
2019-05-10 17:01:15 +0200

    sync TD3 to latest refactoring

--- baselines/td3/logs/default/source_files/engine.py
+++ baselines/td3/logs/default/source_files/engine.py
@@ -16,52 +16,51 @@ class Engine(BaseEngine):
         train_logs = []
         eval_logs = []
         eval_togo = 0
-        checkpoint_togo = 0
+        dump_togo = 0
         num_episode = 0
-        observation = self.env.reset()
+        checkpoint_count = 0
+        observation, _ = self.env.reset()
         for i in count():
             if i >= self.config['train.timestep']:
                 break
-                
             if i < self.config['replay.init_size']:
                 action = [self.env.action_space.sample()]
             else:
                 action = self.agent.choose_action(observation, mode='train')['action']
-            next_observation, reward, done, info = self.env.step(action)
+            next_observation, reward, step_info = self.env.step(action)
             eval_togo += 1
-            checkpoint_togo += 1
-            if done[0]:  # [0] due to single environment
+            dump_togo += 1
+            if step_info[0].last:  # [0] due to single environment
                 start_time = perf_counter()
-                # NOTE: must use latest TimeLimit
-                reach_time_limit = info[0].get('TimeLimit.truncated', False)
-                reach_terminal = not reach_time_limit
-                self.replay.add(observation[0], action[0], reward[0], info[0]['last_observation'], reach_terminal)
+                self.replay.add(observation[0], action[0], reward[0], step_info[0]['last_observation'], step_info[0].terminal)
                 
-                # DDPG updates in the end of episode, for each time step
-                out_agent = self.agent.learn(D=None, replay=self.replay, episode_length=info[0]['episode']['horizon'])
+                # updates in the end of episode, for each time step
+                out_agent = self.agent.learn(D=None, replay=self.replay, episode_length=step_info[0]['episode']['horizon'])
                 num_episode += 1
-                if checkpoint_togo >= self.config['checkpoint.freq']:
-                    checkpoint_togo %= self.config['checkpoint.freq']
+                if (i+1) >= int(self.config['train.timestep']*(checkpoint_count/(self.config['checkpoint.num'] - 1))):
                     self.agent.checkpoint(self.logdir, num_episode)
-                
+                    checkpoint_count += 1
                 logger = Logger()
                 logger('num_seconds', round(perf_counter() - start_time, 1))
                 logger('accumulated_trained_timesteps', i + 1)
                 logger('accumulated_trained_episodes', num_episode)
                 [logger(key, value) for key, value in out_agent.items()]
-                logger('episode_return', info[0]['episode']['return'])
-                logger('episode_horizon', info[0]['episode']['horizon'])
+                logger('episode_return', step_info[0]['episode']['return'])
+                logger('episode_horizon', step_info[0]['episode']['horizon'])
                 train_logs.append(logger.logs)
-                if num_episode == 1 or num_episode % self.config['log.freq'] == 0:
+                if dump_togo >= self.config['log.freq']:
+                    dump_togo %= self.config['log.freq']
                     logger.dump(keys=None, index=0, indent=0, border='-'*50)
-                
                 if eval_togo >= self.config['eval.freq']:
                     eval_togo %= self.config['eval.freq']
                     eval_logs.append(self.eval(accumulated_trained_timesteps=(i+1), 
                                                accumulated_trained_episodes=num_episode))
             else:
-                self.replay.add(observation[0], action[0], reward[0], next_observation[0], done[0])
+                self.replay.add(observation[0], action[0], reward[0], next_observation[0], step_info[0].terminal)
             observation = next_observation
+        if checkpoint_count < self.config['checkpoint.num']:
+            self.agent.checkpoint(self.logdir, num_episode)
+            checkpoint_count += 1
         return train_logs, eval_logs
 
     def eval(self, n=None, **kwargs):
