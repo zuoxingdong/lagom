cfbd5644d338ac343dde743362703e27dd81845c
Xingdong Zuo <zuoxingdong@github.com>
2019-12-13 14:42:52 +0100

    Update checkpointer: Separate out stateful objects

--- lagom/experiment/checkpointer.py
+++ lagom/experiment/checkpointer.py
@@ -1,22 +1,18 @@
 import torch
 
 
-def checkpointer(mode, config, **kwargs):
+def checkpointer(mode, config, obj=[], state_obj=[]):
     assert mode in ['save', 'load']
     if mode == 'save':
-        data = {key: value.state_dict() if hasattr(value, 'state_dict') else value for key, value in kwargs.items()}
+        state_dicts = [x.state_dict() for x in state_obj]
         import pickle
-        torch.save(obj=data, f=config.resume_checkpointer, pickle_protocol=pickle.HIGHEST_PROTOCOL)
+        torch.save(obj=[obj, state_dicts], f=config.resume_checkpointer, pickle_protocol=pickle.HIGHEST_PROTOCOL)
         print(f'$ Checkpoint saved at {config.resume_checkpointer.as_posix()}.')
     elif mode == 'load':
-        data = torch.load(f=config.resume_checkpointer, map_location=config.device)
+        obj, state_dicts = torch.load(f=config.resume_checkpointer, map_location=config.device)
+        for x, x_dict in zip(state_obj, state_dicts):
+            x.load_state_dict(x_dict)
         print(f'$ Checkpoint loaded from {config.resume_checkpointer.as_posix()}.')
-        out = {}
-        for key, value in kwargs.items():
-            if hasattr(value, 'load_state_dict'):
-                value.load_state_dict(data[key])
-            else:
-                out[key] = data[key]
-        return out
+        return obj
     else:
         raise NotImplementedError
