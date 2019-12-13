78934015c5793b498cc3341510834d123674f8f9
Xingdong Zuo <zuoxingdong@github.com>
2019-12-13 19:48:19 +0100

    Sync CMAES

--- baselines/cmaes/agent.py
+++ baselines/cmaes/agent.py
@@ -4,22 +4,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import gym.spaces as spaces
-from lagom import BaseAgent
-from lagom.utils import pickle_dump
-from lagom.utils import numpify
-from lagom.networks import Module
-from lagom.networks import make_fc
-from lagom.networks import CategoricalHead
-from lagom.networks import DiagGaussianHead
+import lagom
+import lagom.utils as utils
+import lagom.rl as rl
 
 
-class MLP(Module):
+class MLP(lagom.nn.Module):
     def __init__(self, config, env, **kwargs):
         super().__init__(**kwargs)
         self.config = config
         self.env = env
         
-        self.feature_layers = make_fc(spaces.flatdim(env.observation_space), config['nn.sizes'])
+        self.feature_layers = lagom.nn.make_fc(spaces.flatdim(env.observation_space), config['nn.sizes'])
         self.layer_norms = nn.ModuleList([nn.LayerNorm(hidden_size) for hidden_size in config['nn.sizes']])
         
     def forward(self, x):
@@ -28,16 +24,16 @@ class MLP(Module):
         return x
 
 
-class Agent(BaseAgent):
+class Agent(lagom.BaseAgent):
     def __init__(self, config, env, **kwargs):
         super().__init__(config, env, **kwargs)
         
         self.feature_network = MLP(config, env, **kwargs)
         feature_dim = config['nn.sizes'][-1]
         if isinstance(env.action_space, spaces.Discrete):
-            self.action_head = CategoricalHead(feature_dim, env.action_space.n, **kwargs)
+            self.action_head = lagom.nn.CategoricalHead(feature_dim, env.action_space.n, **kwargs)
         elif isinstance(env.action_space, spaces.Box):
-            self.action_head = DiagGaussianHead(feature_dim, spaces.flatdim(env.action_space), 0.5, **kwargs)
+            self.action_head = lagom.nn.DiagGaussianHead(feature_dim, spaces.flatdim(env.action_space), 0.5, **kwargs)
         
         self.register_buffer('total_timestep', torch.tensor(0))
         
@@ -47,7 +43,7 @@ class Agent(BaseAgent):
         action_dist = self.action_head(features)
         action = action_dist.sample()
         out = {}
-        out['raw_action'] = numpify(action, self.env.action_space.dtype).squeeze(0)
+        out['raw_action'] = utils.numpify(action.squeeze(0), self.env.action_space.dtype)
         return out
     
     def learn(self, D, **kwargs):
@@ -57,5 +53,5 @@ class Agent(BaseAgent):
         self.save(logdir/f'agent_{num_iter}.pth')
         if 'env.normalize_obs' in self.config and self.config['env.normalize_obs']:
             moments = (self.env.obs_moments.mean, self.env.obs_moments.var)
-            pickle_dump(obj=moments, f=logdir/f'obs_moments_{num_iter}', ext='.pth')
+            utils.pickle_dump(obj=moments, f=logdir/f'obs_moments_{num_iter}', ext='.pth')
 
