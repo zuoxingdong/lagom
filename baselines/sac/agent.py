405f852a789ccb5a6485726a8c8b936425c65f07
Xingdong Zuo <zuoxingdong@github.com>
2020-01-08 14:27:08 +0100

    Sync. SAC

--- baselines/sac/agent.py
+++ baselines/sac/agent.py
@@ -1,4 +1,4 @@
-import numpy as np
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,61 +6,27 @@ import torch.optim as optim
 from torch.distributions import Independent
 from torch.distributions import Normal
 from torch.distributions import TransformedDistribution
-from torch.distributions import Transform
-from torch.distributions import constraints
 
-from gym.spaces import flatdim
-from lagom import BaseAgent
-from lagom.utils import tensorify
-from lagom.utils import numpify
-from lagom.networks import Module
-from lagom.networks import make_fc
-from lagom.networks import ortho_init
-from lagom.transform import describe
-
+import gym.spaces as spaces
+import lagom
+import lagom.utils as utils
 
 # TODO: import from PyTorch when PR merged: https://github.com/pytorch/pytorch/pull/19785
-class TanhTransform(Transform):
-    r"""Transform via the mapping :math:`y = \tanh(x)`."""
-    domain = constraints.real
-    codomain = constraints.interval(-1.0, 1.0)
-    bijective = True
-    sign = +1
+from tanh_transform import TanhTransform
 
-    @staticmethod
-    def atanh(x):
-        return 0.5 * (x.log1p() - (-x).log1p())
 
-    def __eq__(self, other):
-        return isinstance(other, TanhTransform)
-
-    def _call(self, x):
-        return x.tanh()
-
-    def _inverse(self, y):
-        return self.atanh(y)
-
-    def log_abs_det_jacobian(self, x, y):
-        # We use a formula that is more numerically stable, see details in the following link
-        # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
-        return 2. * (np.log(2.) - x - F.softplus(-2. * x))
-    
-
-class Actor(Module):
+class Actor(lagom.nn.Module):
     LOGSTD_MAX = 2
     LOGSTD_MIN = -20
 
-    def __init__(self, config, env, device, **kwargs):
+    def __init__(self, config, env, **kwargs):
         super().__init__(**kwargs)
         self.config = config
         self.env = env
-        self.device = device
         
-        self.feature_layers = make_fc(flatdim(env.observation_space), [256, 256])
-        self.mean_head = nn.Linear(256, flatdim(env.action_space))
-        self.logstd_head = nn.Linear(256, flatdim(env.action_space))
-        
-        self.to(device)
+        self.feature_layers = lagom.nn.make_fc(spaces.flatdim(env.observation_space), [256, 256])
+        self.mean_head = nn.Linear(256, spaces.flatdim(env.action_space))
+        self.logstd_head = nn.Linear(256, spaces.flatdim(env.action_space))
 
     def forward(self, x):
         for layer in self.feature_layers:
@@ -77,64 +43,62 @@ class Actor(Module):
         for layer in self.feature_layers:
             x = F.relu(layer(x))
         mean = self.mean_head(x)
+        mean = torch.tanh(mean)
         return mean
 
 
-class Critic(Module):
-    def __init__(self, config, env, device, **kwargs):
+class Critic(lagom.nn.Module):
+    def __init__(self, config, env, **kwargs):
         super().__init__(**kwargs)
         self.config = config
         self.env = env
-        self.device = device
         
+        obs_action_dim = spaces.flatdim(env.observation_space) + spaces.flatdim(env.action_space)
         # Q1
-        self.first_feature_layers = make_fc(flatdim(env.observation_space) + flatdim(env.action_space), [256, 256])
-        self.first_Q_head = nn.Linear(256, 1)
-        
+        self.Q1_layers = lagom.nn.make_fc(obs_action_dim, [256, 256])
+        self.Q1_head = nn.Linear(256, 1)
         # Q2
-        self.second_feature_layers = make_fc(flatdim(env.observation_space) + flatdim(env.action_space), [256, 256])
-        self.second_Q_head = nn.Linear(256, 1)
-        
-        self.to(self.device)
-        
+        self.Q2_layers = lagom.nn.make_fc(obs_action_dim, [256, 256])
+        self.Q2_head = nn.Linear(256, 1)
+
     def Q1(self, x, action):
         x = torch.cat([x, action], dim=-1)
-        for layer in self.first_feature_layers:
+        for layer in self.Q1_layers:
             x = F.relu(layer(x))
-        x = self.first_Q_head(x)
+        x = self.Q1_head(x)
         return x
     
     def Q2(self, x, action):
         x = torch.cat([x, action], dim=-1)
-        for layer in self.second_feature_layers:
+        for layer in self.Q2_layers:
             x = F.relu(layer(x))
-        x = self.second_Q_head(x)
+        x = self.Q2_head(x)
         return x
         
     def forward(self, x, action):
         return self.Q1(x, action), self.Q2(x, action)
 
 
-class Agent(BaseAgent):
-    def __init__(self, config, env, device, **kwargs):
-        super().__init__(config, env, device, **kwargs)
+class Agent(lagom.BaseAgent):
+    def __init__(self, config, env, **kwargs):
+        super().__init__(config, env, **kwargs)
         
-        self.actor = Actor(config, env, device, **kwargs)
+        self.actor = Actor(config, env, **kwargs)
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=config['agent.actor.lr'])
         
-        self.critic = Critic(config, env, device, **kwargs)
-        self.critic_target = Critic(config, env, device, **kwargs)
+        self.critic = Critic(config, env, **kwargs)
+        self.critic_target = Critic(config, env, **kwargs)
         self.critic_target.load_state_dict(self.critic.state_dict())
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=config['agent.critic.lr'])
         
-        self.target_entropy = -float(flatdim(env.action_space))
-        self.log_alpha = nn.Parameter(torch.tensor(np.log(config['agent.initial_temperature'])).to(device))
+        self.target_entropy = -float(spaces.flatdim(env.action_space))
+        self.log_alpha = nn.Parameter(torch.tensor(math.log(config['agent.initial_temperature'])))
         self.log_alpha_optimizer = optim.Adam([self.log_alpha], lr=config['agent.actor.lr'])
         
-        self.optimizer_zero_grad = lambda: [opt.zero_grad() for opt in [self.actor_optimizer, 
-                                                                        self.critic_optimizer, 
-                                                                        self.log_alpha_optimizer]]
-        self.total_timestep = 0
+        self.zero_grad_all = lambda: [opt.zero_grad() for opt in [self.actor_optimizer, 
+                                                                  self.critic_optimizer, 
+                                                                  self.log_alpha_optimizer]]
+        self.register_buffer('total_timestep', torch.tensor(0))
         
     @property
     def alpha(self):
@@ -146,12 +110,12 @@ class Agent(BaseAgent):
             target_param.data.copy_(p*target_param.data + (1 - p)*param.data)
 
     def choose_action(self, x, **kwargs):
-        obs = tensorify(x.observation, self.device).unsqueeze(0)
+        obs = torch.as_tensor(x.observation).float().to(self.config.device).unsqueeze(0)
         with torch.no_grad():
             if kwargs['mode'] == 'train':
-                action = numpify(self.actor(obs).sample(), 'float')
+                action = utils.numpify(self.actor(obs).sample(), 'float')
             elif kwargs['mode'] == 'eval':
-                action = numpify(torch.tanh(self.actor.mean_forward(obs)), 'float')
+                action = utils.numpify(self.actor.mean_forward(obs), 'float')
         out = {}
         out['raw_action'] = action.squeeze(0)
         return out
@@ -159,15 +123,10 @@ class Agent(BaseAgent):
     def learn(self, D, **kwargs):
         replay = kwargs['replay']
         T = kwargs['T']
-        list_actor_loss = []
-        list_critic_loss = []
-        list_alpha_loss = []
-        Q1_vals = []
-        Q2_vals = []
-        logprob_vals = []
+        list_actor_loss, list_critic_loss, list_alpha_loss, Q1_vals, Q2_vals, logprob_vals = [], [], [], [], [], []
         for i in range(T):
-            observations, actions, rewards, next_observations, masks = replay.sample(self.config['replay.batch_size'])
-            
+            samples = replay.sample(self.config['replay.batch_size'])
+            observations, actions, next_observations, rewards, masks = map(lambda x: torch.as_tensor(x).to(self.config.device), samples)
             Qs1, Qs2 = self.critic(observations, actions)
             with torch.no_grad():
                 action_dist = self.actor(next_observations)
@@ -177,7 +136,7 @@ class Agent(BaseAgent):
                 next_Qs = torch.min(next_Qs1, next_Qs2) - self.alpha.detach()*next_actions_logprob
                 targets = rewards + self.config['agent.gamma']*masks*next_Qs
             critic_loss = F.mse_loss(Qs1, targets.detach()) + F.mse_loss(Qs2, targets.detach())
-            self.optimizer_zero_grad()
+            self.zero_grad_all()
             critic_loss.backward()
             critic_grad_norm = nn.utils.clip_grad_norm_(self.critic.parameters(), self.config['agent.max_grad_norm'])
             self.critic_optimizer.step()
@@ -188,13 +147,13 @@ class Agent(BaseAgent):
             actor_Qs1, actor_Qs2 = self.critic(observations, policy_actions)
             actor_Qs = torch.min(actor_Qs1, actor_Qs2)
             actor_loss = torch.mean(self.alpha.detach()*policy_actions_logprob - actor_Qs)
-            self.optimizer_zero_grad()
+            self.zero_grad_all()
             actor_loss.backward()
             actor_grad_norm = nn.utils.clip_grad_norm_(self.actor.parameters(), self.config['agent.max_grad_norm'])
             self.actor_optimizer.step()
             
             alpha_loss = torch.mean(self.log_alpha*(-policy_actions_logprob - self.target_entropy).detach())
-            self.optimizer_zero_grad()
+            self.zero_grad_all()
             alpha_loss.backward()
             self.log_alpha_optimizer.step()
 
@@ -208,15 +167,15 @@ class Agent(BaseAgent):
         self.total_timestep += T
         
         out = {}
-        out['actor_loss'] = torch.tensor(list_actor_loss).mean(0).item()
+        out['actor_loss'] = torch.as_tensor(list_actor_loss).mean(0).item()
         out['actor_grad_norm'] = actor_grad_norm
-        out['critic_loss'] = torch.tensor(list_critic_loss).mean(0).item()
+        out['critic_loss'] = torch.as_tensor(list_critic_loss).mean(0).item()
         out['critic_grad_norm'] = critic_grad_norm
-        describe_it = lambda x: describe(numpify(torch.cat(x), 'float').squeeze(), axis=-1, repr_indent=1, repr_prefix='\n')
+        describe_it = lambda x: utils.describe(torch.cat(x).squeeze(), axis=-1, repr_indent=1, repr_prefix='\n')
         out['Q1'] = describe_it(Q1_vals)
         out['Q2'] = describe_it(Q2_vals)
         out['logprob'] = describe_it(logprob_vals)
-        out['alpha_loss'] = torch.tensor(list_alpha_loss).mean(0).item()
+        out['alpha_loss'] = torch.as_tensor(list_alpha_loss).mean(0).item()
         out['alpha'] = self.alpha.item()
         return out
     
--- baselines/sac/experiment.py
+++ baselines/sac/experiment.py
@@ -1,29 +1,18 @@
-import os
 import gym
+import gym.wrappers
 
-from lagom import EpisodeRunner
-from lagom import RandomAgent
-from lagom.utils import pickle_dump
-from lagom.utils import set_global_seeds
-from lagom.experiment import Config
-from lagom.experiment import Grid
-from lagom.experiment import run_experiment
-from lagom.envs import RecordEpisodeStatistics
-from lagom.envs import TimeStepEnv
-
-# TODO: replace it with official gym wrapper
-from .normalize_action import NormalizeAction
+import lagom
+import lagom.utils as utils
 
 from baselines.sac.agent import Agent
 from baselines.sac.engine import Engine
-from baselines.sac.replay_buffer import ReplayBuffer
 
 
-config = Config(
+configurator = lagom.Configurator(
     {'log.freq': 10,
-     'checkpoint.num': 3,
+     'checkpoint.inference.num': 3,
      
-     'env.id': Grid(['HalfCheetah-v3', 'Hopper-v3', 'Walker2d-v3', 'Swimmer-v3']),
+     'env.id': lagom.Grid(['HalfCheetah-v3', 'Hopper-v3', 'Walker2d-v3']),
      
      'agent.gamma': 0.99,
      'agent.polyak': 0.995,  # polyak averaging coefficient for targets update
@@ -32,53 +21,72 @@ config = Config(
      'agent.critic.lr': 3e-4,
      'agent.critic.use_lr_scheduler': False,
      'agent.initial_temperature': 1.0,
-     'agent.max_grad_norm': 999999,  # grad clipping by norm
+     'agent.max_grad_norm': 1e5,  # grad clipping by norm
      
-     'replay.capacity': 1000000, 
+     'replay.capacity': int(1e6), 
      'replay.init_trial': 10,  # number of random rollouts initially
      'replay.batch_size': 256,
      
-     'train.timestep': int(1e6),  # total number of training (environmental) timesteps
+     'train.timestep': int(1e6),  # total number of training (environment) timesteps
      'eval.num': 200
-    })
+    }, 
+    num_sample=1)
 
 
-def make_env(config, seed, mode):
+def make_env(config, mode):
     assert mode in ['train', 'eval']
     env = gym.make(config['env.id'])
-    env.seed(seed)
-    env.observation_space.seed(seed)
-    env.action_space.seed(seed)
-    env = NormalizeAction(env)  # TODO: use gym new wrapper RescaleAction when it's merged
+    env.seed(config.seed)
+    env.observation_space.seed(config.seed)
+    env.action_space.seed(config.seed)
+    env = gym.wrappers.RescaleAction(env, -1.0, 1.0)
     if mode == 'eval':
-        env = RecordEpisodeStatistics(env, deque_size=100)
-    env = TimeStepEnv(env)
+        env = lagom.envs.RecordEpisodeStatistics(env, deque_size=100)
+    env = lagom.envs.TimeStepEnv(env)
     return env
 
 
-def run(config, seed, device, logdir):
-    set_global_seeds(seed)
+def run(config):
+    lagom.set_global_seeds(config.seed)
     
-    env = make_env(config, seed, 'train')
-    eval_env = make_env(config, seed, 'eval')
-    random_agent = RandomAgent(config, env, device)
-    agent = Agent(config, env, device)
-    runner = EpisodeRunner()
-    replay = ReplayBuffer(env, config['replay.capacity'], device)
-    engine = Engine(config, agent=agent, random_agent=random_agent, env=env, eval_env=eval_env, runner=runner, replay=replay, logdir=logdir)
+    env = make_env(config, 'train')
+    eval_env = make_env(config, 'eval')
+    agent = Agent(config, env)
+    runner = lagom.EpisodeRunner()
+    replay = lagom.UniformTransitionBuffer(env, config['replay.capacity'])
+    engine = Engine(config, agent=agent)
     
-    train_logs, eval_logs = engine.train()
-    pickle_dump(obj=train_logs, f=logdir/'train_logs', ext='.pkl')
-    pickle_dump(obj=eval_logs, f=logdir/'eval_logs', ext='.pkl')
-    return None  
+    cond_save = utils.NConditioner(max_n=config['train.timestep'], num_conditions=config['checkpoint.inference.num'], mode='accumulative')
+    cond_eval = utils.NConditioner(max_n=config['train.timestep'], num_conditions=config['eval.num'], mode='accumulative')
+    train_logs, eval_logs = [], []
+    iteration = 0
+    if config.resume_checkpointer.exists():
+        out = lagom.checkpointer('load', config, state_obj=[agent, agent.actor_optimizer, agent.critic_optimizer, agent.log_alpha_optimizer])
+        env, eval_env, runner, train_logs, eval_logs, iteration = out
+        agent.env = env
     
+    while agent.total_timestep < config['train.timestep']:
+        train_logger = engine.train(iteration, env=env, runner=runner, replay=replay)
+        train_logs.append(train_logger.logs)
+        if cond_save(int(agent.total_timestep)):
+            agent.checkpoint(config.logdir, iteration+1)
+        if cond_eval(int(agent.total_timestep)):
+            eval_logger = engine.eval(iteration, eval_env=eval_env, runner=runner)
+            eval_logs.append(eval_logger.logs)
+        utils.pickle_dump(obj=train_logs, f=config.logdir/'train_logs', ext='.pkl')
+        utils.pickle_dump(obj=eval_logs, f=config.logdir/'eval_logs', ext='.pkl')
+        lagom.checkpointer('save', config, obj=[env, eval_env, runner, train_logs, eval_logs, iteration+1], state_obj=[agent, agent.actor_optimizer, agent.critic_optimizer, agent.log_alpha_optimizer])
+        iteration += 1
+    agent.checkpoint(config.logdir, iteration+1)    
+    return None
+
 
 if __name__ == '__main__':
-    run_experiment(run=run, 
-                   config=config, 
-                   seeds=[4153361530, 3503522377, 2876994566, 172236777, 3949341511], 
-                   log_dir='logs/default',
-                   max_workers=os.cpu_count(), 
-                   chunksize=1, 
-                   use_gpu=True,  # GPU much faster, note that performance differs between CPU/GPU
-                   gpu_ids=None)
+    lagom.run_experiment(run=run, 
+                         configurator=configurator, 
+                         seeds=lagom.SEEDS[:5],
+                         log_dir='logs/default',
+                         max_workers=None,
+                         chunksize=1, 
+                         use_gpu=False,  # GPU much faster, note that performance differs between CPU/GPU
+                         gpu_ids=None)
