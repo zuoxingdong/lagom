b5143c60d0954c3452a8e62f38bc5a0e1c57481c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 05:52:35 +0000

    Adding tarfile member sanitization to extractall()

--- baselines/unzip_logs.py
+++ baselines/unzip_logs.py
@@ -13,7 +13,29 @@ if all_logs_path.exists():
     shutil.rmtree(all_logs_path)
 print('Extracting all logs folders...')
 with tarfile.open('all_logs.tar.gz') as tar:
-    tar.extractall()
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar)
 assert all_logs_path.exists()
 print('Done !')
 print('Moving all logs folders...')
